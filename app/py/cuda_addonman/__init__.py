1686a273523cb3fd24b5aaa3f1360592300a4236
Alexey-T <support@uvviewsoft.com>
2015-09-26 02:53:35 +0300

    py pluginman

--- app/py/cuda_addonman/__init__.py
+++ app/py/cuda_addonman/__init__.py
@@ -1,8 +1,21 @@
+import os
 from cudatext import *
+from .workremote import get_url, get_item_url, get_avail_list, get_plugin_zip
 
 class Command:
-    def do_edit(self):
-        msg_box('edit', MB_OK)
-        msg_status('edit')
+    def do_install(self):
+        #get only lexer items
+        items = get_avail_list()
+        items = [l for l in items if l[0].startswith('Lexer:')]
+        
+        text = '\n'.join(l[0] for l in items)
+        res = dlg_menu(MENU_LIST, text)
+        if res is None: return
+        res = items[res]
+        url = get_item_url(res[2])
+        fn = get_plugin_zip(url)
+        if not os.path.isfile(fn): return
+        file_open(fn) 
+        
     def do_remove(self):
         msg_box('remov', MB_OK)
