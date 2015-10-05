c2ba9f23dbe2a6d70b49b55ee81e75f52c5c01cb
Alexey-T <support@uvviewsoft.com>
2015-10-06 00:04:59 +0300

    plugin

---
+++ app/py/cuda_carets_num/__init__.py
@@ -0,0 +1,40 @@
+from cudatext import *
+
+class Command:
+    def run(self):
+        carets = ed.get_carets()
+        if not carets: 
+            msg_box('Place several carets first', MB_OK)
+            return
+
+        s = dlg_input_ex(4, 'Carets numbering', 
+          'Starting number:', '1', 
+          'Digits:', '1',
+          'Text before numbers:', '',
+          'Text after numbers:', '')
+        if not s: return
+        try:
+            n_start = int(s[0])
+        except:
+            msg_box('Incorrect number entered: '+s[0], MB_OK)
+            return
+        try:
+            n_len = int(s[1])
+        except:
+            msg_box('Incorrect number entered: '+s[1], MB_OK)
+            return
+            
+        text_before = s[2]
+        text_after = s[3]
+        
+        carets = list(reversed(carets))
+        
+        ed.lock()
+        for i, caret in enumerate(carets):
+            num = (len(carets)-i+n_start-1)
+            text = text_before + ('%0'+str(n_len)+'d') % num + text_after
+            ed.insert(caret[0], caret[1], text)
+        ed.unlock()
+            
+        msg_status('Inserted %d numbers' % len(carets))
+        
