f7787f80fa2319054aa2d1bd0cad10e25691ffbb
Alexey-T <support@uvviewsoft.com>
2015-10-29 20:03:57 +0300

    upd

--- app/proc_cmd.pas
+++ app/proc_cmd.pas
@@ -222,7 +222,7 @@ begin
   M.Add(cmd_CommentLineRemove, 'comments: remove line comment', [], []);
   M.Add(cmd_CommentLineToggle_AtNonspace, 'comments: toggle line comment, at non-space char', [cXControl+'+/'], []);
   M.Add(cmd_CommentLineToggle_AtStart, 'comments: toggle line comment, at line start', [], []);
-  M.Add(cmd_CommentStreamToggle, 'comments: toggle stream comment', [], []);
+  M.Add(cmd_CommentStreamToggle, 'comments: toggle stream comment', ['Alt+/'], []);
   M.Add(cmd_DuplicateLineEx, 'duplicate line (advanced)', [], []);
 
   M.Add(cmd_SplitTabToggle, 'split tab: toggle split', [], []);
