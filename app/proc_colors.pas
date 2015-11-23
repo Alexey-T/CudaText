e6438f888e911f935d303aec2a6a25c322442b8b
Alexey-T <support@uvviewsoft.com>
2015-11-23 20:42:03 +0300

    renam colors

--- app/proc_colors.pas
+++ app/proc_colors.pas
@@ -103,8 +103,8 @@ begin
   Add(clMedGray, 'EdBlockStaple', 'editor, block staples (indent guides)');
   Add(clGray, 'EdComboArrow', 'editor, combobox arrow-down');
   Add($f0f0f0, 'EdComboArrowBg', 'editor, combobox arrow-down BG');
-  Add(clMedGray, 'EdBlockSepLine', 'editor, folded block, separator line');
-  Add($a06060, 'EdFoldMarkLine', 'editor, folded block mark, border');
+  Add(clMedGray, 'EdBlockSepLine', 'editor, separator line');
+  Add($a06060, 'EdFoldMarkLine', 'editor, fully folded line');
   Add($e08080, 'EdFoldMarkFont', 'editor, folded block mark, font');
   Add(clCream, 'EdFoldMarkBg', 'editor, folded block mark, BG');
   Add(clGray, 'EdGutterFont', 'editor, gutter font');
--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -15,7 +15,7 @@ uses
   Classes, SysUtils;
 
 const
-  cAppExeVersion = '1.0.2.2';
+  cAppExeVersion = '1.0.4.0';
   cAppApiVersion = '1.0.109';
 
 
