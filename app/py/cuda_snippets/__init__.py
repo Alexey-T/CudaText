2dd1aadf8b359cd97505db5aee6fbe187d0f5747
Alexey-T <support@uvviewsoft.com>
2015-11-28 02:09:17 +0300

    snips

--- app/py/cuda_snippets/__init__.py
+++ app/py/cuda_snippets/__init__.py
@@ -16,25 +16,39 @@ class Command:
     def do_load_snippets(self):
         dir = os.path.join(app_path(APP_DIR_DATA), 'snippets')
         self.snips = get_snip_list_of_dicts(dir)
+        
+    def get_snip_list_current(self):
+        lex = ed.get_prop(PROP_LEXER_CARET)
+        return [item for item in self.snips
+                if is_lexer_listed(lex, item[SNIP_LEX]) ]
+    
     
     def on_key(self, ed_self, code, state):
         if code!=9: return #tab-key=9
         if state!='': return
         if ed_self.get_prop(PROP_TAB_COLLECT_MARKERS): return
+        
+        carets = ed_self.get_carets()
+        if len(carets)!=1: return
+        
         name = get_snip_name_from_editor(ed_self)
         if not name: return
-        print('snippet:', name)
-    
+        
+        items = self.get_snip_list_current()
+        for item in items:
+            if item[SNIP_ID]==name:
+                print('snippet:', name)
+                x0, y0, x1, y1 = carets[0]
+                ed_self.delete(x0-len(name), y0, x0, y0)
+                ed_self.set_caret(x0-len(name), y0)
+                insert_snip_into_editor(ed_self, item[SNIP_TEXT])
+                return False #block tab-key
+                
+
     def do_menu(self):
-        lex = ed.get_prop(PROP_LEXER_CARET)
-        
-        snips_here = [item for item in self.snips
-                      if is_lexer_listed(lex, item[SNIP_LEX]) ]
-        
+        items = self.get_snip_list_current()
         names = [item[SNIP_NAME]+'\t'+item[SNIP_ID]+'  ['+item[SNIP_LEX]+']' 
-                for item in snips_here]
+                for item in items]
         res = dlg_menu(MENU_LIST, '\n'.join(names))
         if res is None: return
-
-        #print('selected:', snips_here[res][SNIP_NAME])
-        insert_snip_into_editor(ed, snips_here[res][SNIP_TEXT])
+        insert_snip_into_editor(ed, items[res][SNIP_TEXT])
