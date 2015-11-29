2510335e67607b0b6463ab6e07a2dddcac22b3b9
Alexey-T <support@uvviewsoft.com>
2015-11-29 21:51:45 +0300

    wiki

--- app/cudatext.app/Contents/Resources/py/cudatext_cmd.py
+++ app/cudatext.app/Contents/Resources/py/cudatext_cmd.py
@@ -233,10 +233,12 @@ cmd_CopyLine         = 2650
 cmd_CopyFilenameFull = 2651
 cmd_CopyFilenameDir  = 2652
 cmd_CopyFilenameName = 2653
+
 cmd_SortAsc          = 2654
 cmd_SortDesc         = 2655
-cmd_SortNocaseAsc    = 2656
-cmd_SortNocaseDesc   = 2657
+
+cmd_ConvertTabsToSpaces = 2658
+cmd_ConvertSpacesToTabsLeading = 2659
 
 cmd_BookmarkToggle    = 2661
 cmd_BookmarkInvertAll = 2662
