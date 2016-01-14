48f1f0126edf8a51983a735af405494b2f64b528
Alexey-T <support@uvviewsoft.com>
2016-01-15 02:35:53 +0300

    mac up

--- app/cudatext.app/Contents/Resources/py/cuda_make_plugin/dlg.py
+++ app/cudatext.app/Contents/Resources/py/cuda_make_plugin/dlg.py
@@ -26,27 +26,34 @@ def get_module_dir():
 def dlg_make_plugin():
     '''
     (s_caption, s_module, cmd_list, event_list)
+    cmd_list[i] is 3-tuple: (caption, method, no_menu)
     '''
     dlg_w = 406
     dlg_h = 460
     btn_w = 80
+    
     id_name = 1
     id_module = 3
     id_items = 5
     id_events = 7
     id_ok = 8
     c1 = chr(1)
+    
+    s_caption = 'MySample'
+    s_module = get_module_dir()
+    s_plugin_items = 'MySample>run'
+    s_events_checks = ''
+    
     while True:
-        dir = get_module_dir()
         res = dlg_custom('Make Plugin', dlg_w, dlg_h, '\n'.join([]
-          + [c1.join(['type=label', 'cap=Plugin &name:', 'pos=6,6,400,0'])]
-          + [c1.join(['type=edit', 'val=MySample', 'pos=6,24,400,46'])]
-          + [c1.join(['type=label', 'cap=Lowercase &module name:', 'pos=6,54,400,0'])]
-          + [c1.join(['type=edit', 'val='+dir, 'pos=6,72,400,46'])]
-          + [c1.join(['type=label', 'cap=Menu-&items in form "Caption>method":', 'pos=6,102,400,0'])]
-          + [c1.join(['type=memo', 'val=MySample>run', 'pos=6,120,400,240'])]
-          + [c1.join(['type=label', 'cap=&Events to handle:', 'pos=6,246,400,0'])]
-          + [c1.join(['type=checklistbox', 'items='+'\t'.join(EVENTS), 'pos=6,264,400,420'])]
+          + [c1.join(['type=label', 'cap=Plugin &name', 'pos=6,6,400,0'])]
+          + [c1.join(['type=edit', 'val='+s_caption, 'pos=6,24,400,46'])]
+          + [c1.join(['type=label', 'cap=Lowercase &module name', 'pos=6,54,400,0'])]
+          + [c1.join(['type=edit', 'val='+s_module, 'pos=6,72,400,46'])]
+          + [c1.join(['type=label', 'cap=&Items "Caption>method" ("-" at end: hide item in Plugins)', 'pos=6,102,400,0'])]
+          + [c1.join(['type=memo', 'val='+s_plugin_items, 'pos=6,120,400,240'])]
+          + [c1.join(['type=label', 'cap=&Events to handle', 'pos=6,246,400,0'])]
+          + [c1.join(['type=checklistbox', 'items='+'\t'.join(EVENTS), 'val='+s_events_checks, 'pos=6,264,400,420'])]
           + [c1.join(['type=button', 'cap=&OK', 'props=1', 'pos=%d,%d,%d,%d'%(dlg_w-btn_w*2-12, dlg_h-30, dlg_w-btn_w-12, 0)])]
           + [c1.join(['type=button', 'cap=Cancel', 'pos=%d,%d,%d,%d'%(dlg_w-btn_w-6, dlg_h-30, dlg_w-6, 0)])]
           ) )
@@ -57,10 +64,13 @@ def dlg_make_plugin():
     
         s_caption = text[id_name]
         s_module = text[id_module]
-        s_items = text[id_items].split('\t')
-        cmd_list = [s.split('>') for s in s_items if s]
-        s_items = text[id_events].split(';')[1].split(',')
-        event_list = [s for (n, s) in enumerate(EVENTS) if s_items[n]=='1']
+        s_plugin_items = text[id_items]
+        s_events_checks = text[id_events]
+        
+        items = text[id_items].split('\t')
+        cmd_list = [s.split('>') for s in items if s]
+        items = text[id_events].split(';')[1].split(',')
+        event_list = [s for (n, s) in enumerate(EVENTS) if items[n]=='1']
         
         if not s_module or not s_caption:
             msg_box('Empty field', MB_OK+MB_ICONERROR)
@@ -72,19 +82,30 @@ def dlg_make_plugin():
         bad = False
         for i in cmd_list:
             if len(i)!=2:
-                msg_box('Incorrect menu-item: '+repr(i), MB_OK+MB_ICONERROR)
+                msg_box('Incorrect item: '+repr(i), MB_OK+MB_ICONERROR)
                 bad = True
                 break
+                
             if not i[0]:
-                msg_box('Incorrect menu-item name: '+i[0], MB_OK+MB_ICONERROR)
+                msg_box('Incorrect item name: '+i[0], MB_OK+MB_ICONERROR)
                 bad = True
                 break
-            if not is_correct_id(i[1]):
-                msg_box('Incorrect menu-item method: '+i[1], MB_OK+MB_ICONERROR)
+                
+            s = i[1]
+            if s.endswith('-'): s=s[:-1]
+            if not is_correct_id(s):
+                msg_box('Incorrect item method: '+s, MB_OK+MB_ICONERROR)
                 bad = True
                 break
 
         if bad:
             continue
                 
+        #handle "-" at end of cmd:
+        for i in cmd_list:
+            nomenu = i[1].endswith('-')
+            i += [nomenu]
+            if nomenu:
+                i[1] = i[1][:-1]
+
         return (s_caption, 'cuda_'+s_module, cmd_list, event_list)
