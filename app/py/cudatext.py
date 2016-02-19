06ec293d844d379bba1fbf348ec4de658d1133a8
Alexey-T <support@uvviewsoft.com>
2016-02-19 13:41:31 +0300

    py ed.bookmark: remov 1 param

--- app/py/cudatext.py
+++ app/py/cudatext.py
@@ -361,8 +361,8 @@ class Editor:
         return ct.ed_cmd(self.h, code, text)
     def focus(self):
         return ct.ed_focus(self.h)
-    def bookmark(self, id, nline, nkind=1, ncolor=-1, icon='', hint=''):
-        return ct.ed_bookmark(self.h, id, nline, nkind, ncolor, icon, hint)
+    def bookmark(self, id, nline, nkind=1, ncolor=-1, text=''):
+        return ct.ed_bookmark(self.h, id, nline, nkind, ncolor, text)
 
     def lock(self):
         return ct.ed_lock(self.h)
