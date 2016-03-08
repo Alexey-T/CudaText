b8b83e32c8121c0bb8e6b6bcd5faa465e6d0369a
Alexey-T <support@uvviewsoft.com>
2016-03-08 16:38:55 +0300

    lang work: localize opened tab0

--- comp/ColorPalette/lazcolorpalette.pas
+++ comp/ColorPalette/lazcolorpalette.pas
@@ -1,21 +1,22 @@
 { This file was automatically created by Lazarus. Do not edit!
-This source is only used to compile and install the package.
+  This source is only used to compile and install the package.
  }
 
-unit LazColorPalette; 
+unit LazColorPalette;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
-  ColorPalette, LazarusPackageIntf; 
+  ColorPalette, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('ColorPalette', @ColorPalette.Register); 
-end; 
+  RegisterUnit('ColorPalette', @ColorPalette.Register);
+end;
 
 initialization
-  RegisterPackage('LazColorPalette', @Register); 
+  RegisterPackage('LazColorPalette', @Register);
 end.
--- comp/UniqInstance/uniqueinstance_package.pas
+++ comp/UniqInstance/uniqueinstance_package.pas
@@ -4,6 +4,7 @@
 
 unit uniqueinstance_package;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
