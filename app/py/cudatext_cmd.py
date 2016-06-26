1e65ae362dba22793d9bb23bec8c3851e9e74ef2
Alexey-T <support@uvviewsoft.com>
2016-06-27 01:28:12 +0300

    cCommand_ToggleMicromap

--- app/py/cudatext_cmd.py
+++ app/py/cudatext_cmd.py
@@ -110,6 +110,7 @@ cCommand_ToggleLineNums = 307
 cCommand_ToggleFolding = 308
 cCommand_ToggleRuler = 309
 cCommand_ToggleMinimap = 310
+cCommand_ToggleMicromap = 311
 
 cCommand_ClipboardPaste = 1000
 cCommand_ClipboardPaste_Select = 1001
