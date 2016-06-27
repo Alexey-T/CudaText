3fd18b57420750322e7abd85f46a59af9cebf0b8
Alexey-T <support@uvviewsoft.com>
2016-06-27 12:05:17 +0300

    Addonman- chanl for Kvichans

--- app/py/cuda_addonman/__init__.py
+++ app/py/cuda_addonman/__init__.py
@@ -16,6 +16,7 @@ ch_user = []
 ch_def = [
   'https://raw.githubusercontent.com/Alexey-T/CudaText-registry/master/registry-addons.txt',
   'https://raw.githubusercontent.com/Alexey-T/CudaText-registry/master/registry-lexers.txt',
+  'https://raw.githubusercontent.com/kvichans/CudaText-registry/master/registry-addons.txt',
   ]
   
 
