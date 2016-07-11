3d4f8a824f7924720a191c95b04f70ca79e1a40d
Alexey-T <support@uvviewsoft.com>
2016-07-11 18:35:45 +0300

    check if filetext saved in sess/ file chg on disk

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -133,6 +133,7 @@ const
   msgConfirmCloseDelFile: string = 'Close tab and delete its file?';
   msgConfirmDeleteLexer: string = 'Delete lexer "%s"?';
   msgConfirmRemoveStylesFromBackup: string = 'Remove checked styles from backup file?';
+  msgConfirmReloadInSessIfChanged: string = 'File text was saved in session, but file modification date was changed:'#10'%s'#10#10'Reload file from disk?';
 
   msgAboutCredits =
       'Lazarus IDE'+sLineBreak+
