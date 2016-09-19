33bd7391d31d95fd791c6a55d3a1791cd576458f
Alexey-t <support@uvviewsoft.com>
2016-09-19 03:48:38 +0300

    up mac

--- app/cudatext.app/Contents/Resources/py/cudatext_cmd.py
+++ app/cudatext.app/Contents/Resources/py/cudatext_cmd.py
@@ -261,8 +261,8 @@ cmd_CopyFilenameFull = 2651
 cmd_CopyFilenameDir  = 2652
 cmd_CopyFilenameName = 2653
 
-cmd_SortAsc          = 2654
-cmd_SortDesc         = 2655
+cmd_SortAsc          = 2654 #deleted
+cmd_SortDesc         = 2655 #deleted
 
 cmd_ToggleTabUsesSpaces = 2657
 cmd_ConvertTabsToSpaces = 2658
@@ -275,12 +275,12 @@ cmd_BookmarkGotoNext  = 2664
 cmd_BookmarkGotoPrev  = 2665
 cmd_BookmarkPlaceCarets = 2667
 
-cmd_CommentLineAdd_AtNonspace    = 2670
-cmd_CommentLineAdd_AtStart       = 2671
-cmd_CommentLineRemove            = 2672
-cmd_CommentLineToggle_AtNonspace = 2673
-cmd_CommentLineToggle_AtStart    = 2674
-cmd_CommentStreamToggle          = 2675
+cmd_CommentLineAdd_AtNonspace    = 2670 #deleted
+cmd_CommentLineAdd_AtStart       = 2671 #deleted
+cmd_CommentLineRemove            = 2672 #deleted
+cmd_CommentLineToggle_AtNonspace = 2673 #deleted
+cmd_CommentLineToggle_AtStart    = 2674 #deleted
+cmd_CommentStreamToggle          = 2675 #deleted
 cmd_DuplicateLineEx              = 2676
   
 cmd_LineEndWin        = 2677
--- app/cudatext.app/Contents/Resources/py/cudax_lib/__init__.py
+++ app/cudatext.app/Contents/Resources/py/cudax_lib/__init__.py
@@ -5,20 +5,13 @@ Overridden option tools:
         Path: simple value (e.g. "tab_size") or "/"-separated path inside JSON tree
     set_opt(path, value, ed=ed, level=CONFIG_LEV_USER)
         Add/Update/Delete option into a config (user-config if no param level).
-Comments:
-    cmt_toggle_stream
-        Add/Remove stream comment (as /*....*/) for cur selection or for cur line (by opt)
-    cmt_toggle_line_1st
-        Add/Remove comment at 1st pos of line
-    cmt_toggle_line_body
-        Add/Remove comment at 1st pos of text
 Duplicate:
     duplicate
         Dub cur selection or cur line (by opt)
 Authors:
     Andrey Kvichansky    (kvichans on github)
 Version:
-    '0.5.7 2016-08-01'
+    '0.6.1 2016-09-13'
 Wiki: github.com/kvichans/cudax_lib/wiki
 ToDo: (see end of file)
 '''
@@ -41,11 +34,8 @@ LAST_FILE_OPTS      = {}
 CONFIG_MSG_DONT_SET_FILE= 'Cannot set editor properties'
 CONFIG_MSG_DONT_SET_PATH= 'Cannot set complex path'
 NEED_NEWER_API          = 'Needs newer app version'
-COMMENTING              = 'Commenting'
 DUPLICATION             = 'Duplication'
 ONLY_NORM_SEL_MODE      = '{} works only with normal selection'
-CMT_NO_LINE_4LEX        = 'No line comment for lexer "{}"'
-CMT_NO_STRM_4LEX        = 'No stream comment for lexer "{}"'
 ONLY_SINGLE_CRT         = "{} doesn't work with multi-carets"
 
 pass;                           # Logging
@@ -56,239 +46,6 @@ pass;                           LOG = (-2==-2)  # Do or dont logging.
 pass;                           log_gap = ''    # use only into log()
 
 class Command:
-    ###############################################
-    ## Comments
-    def cmt_toggle_line_1st(self):
-        return self._cmt_toggle_line('1st')
-    def cmt_toggle_line_body(self):
-        return self._cmt_toggle_line('bod')
-    def _cmt_toggle_line(self, cmt_type, ed_=ed):
-        ''' Add/Remove full line comment
-            Params
-                cmt_type    '1st'    at begin of line
-                            'bod'    at first not blank
-        '''
-        if not _check_API('1.0.108'):    return
-        lex         = ed_.get_prop(app.PROP_LEXER_CARET)
-        cmt_sgn     = app.lexer_proc(app.LEXER_GET_COMMENT, lex)
-        pass;                  #LOG and log('cmt_type, lex, cmt_sgn={}', (cmt_type, lex, cmt_sgn))
-        if not cmt_sgn:
-            return app.msg_status(CMT_NO_LINE_4LEX.format(lex))
-        # Analize
-        bEmpSel     = False
-        rWrks       = []
-        pass;                  #LOG and log('ed_.get_sel_mode(),app.SEL_NORMAL,app.SEL_COLUMN={}', (ed_.get_sel_mode(),app.SEL_NORMAL,app.SEL_COLUMN))
-        crts        = ed_.get_carets()
-        if False:pass
-        elif ed_.get_sel_mode() == app.SEL_NORMAL:
-            bEmpSel     = 1==len(crts) and -1==crts[0][3]
-            for (cCrt, rCrt ,cEnd, rEnd) in crts:
-                (rCrtMin
-                ,rCrtMax)   = minmax(rCrt, rEnd if -1!=rEnd else rCrt)
-                if -1!=rEnd and rCrt>rEnd and 0==cCrt:
-                    rCrtMax = rCrtMax-1    # For direct section along left bound
-                rWrks      += list(range(rCrtMin, rCrtMax+1))
-        elif ed_.get_sel_mode() == app.SEL_COLUMN:
-            (cBgn
-            ,rSelBgn
-            ,cEnd
-            ,rSelEnd)   = ed_.get_sel_rect()
-            rWrks       = list(range(rSelBgn, rSelEnd+1))
-        if not rWrks:
-            rWrks       = [crts[0][1]]
-        do_uncmt    = ed_.get_text_line(rWrks[0]).lstrip().startswith(cmt_sgn)
-        # Work
-        save_bd_col = get_opt('comment_save_column' , False)
-        at_min_bd   = get_opt('comment_equal_column', False)
-        col_min_bd  = 1000 # infinity
-        if at_min_bd:
-            for rWrk in rWrks:
-                line        = ed_.get_text_line(rWrk)
-                pos_body    = line.index(line.lstrip())
-                pos_body    = len(line) if 0==len(line.lstrip()) else pos_body
-                col_min_bd  = min(pos_body, col_min_bd)
-                if 0==col_min_bd:
-                    break # for rWrk
-        blnks4cmt   = ' '*len(cmt_sgn) # '\t'.expandtabs(len(cmt_sgn))
-        pass;                  #LOG and log('rWrks,do_uncmt, save_cols, at_min_bd, col_min_bd={}', (rWrks,do_uncmt,save_bd_col,at_min_bd,col_min_bd))
-        for rWrk in rWrks:
-            line    = ed_.get_text_line(rWrk)
-            pos_body= line.index(line.lstrip())
-            pos_body= len(line) if 0==len(line.lstrip()) else pos_body
-            pass;              #LOG and log('rWrk,pos_body,line={}', (rWrk,pos_body,line))
-            if do_uncmt:
-                # Uncomment!
-                if not line[pos_body:].startswith(cmt_sgn):
-                    # Already no comment
-                    continue    #for rWrk
-                if False:pass
-                elif len(line)==len(cmt_sgn): # and line.startswith(cmt_sgn)
-                    line = ''
-                elif save_bd_col and (' '==line[0] or
-                                      ' '==line[pos_body+len(cmt_sgn)]):
-                    # Before or after cmt_sgn must be blank
-                    line = line.replace(cmt_sgn, blnks4cmt, 1)
-                else:
-                    line = line.replace(cmt_sgn, ''       , 1)
-            else:
-                # Comment!
-                if cmt_type=='bod' and line[pos_body:].startswith(cmt_sgn):
-                    # Body comment already sets - willnot double it
-                    continue    #for rWrk
-                if False:pass
-                elif cmt_type=='1st' and save_bd_col and line.startswith(blnks4cmt) :
-                    line = line.replace(blnks4cmt, cmt_sgn, 1)
-               #elif cmt_type=='1st' and save_bd_col #  !line.startswith(blnks4cmt) :
-                elif cmt_type=='1st':#  !save_bd_col
-                    line = cmt_sgn+line
-                elif cmt_type=='bod' and save_bd_col and line.startswith(blnks4cmt):
-                    pos_cmnt = col_min_bd if at_min_bd else pos_body
-                    pass;          #LOG and log('pos_cmnt={}', (pos_cmnt))
-                    if pos_cmnt>=len(cmt_sgn):
-                        line = line[:pos_cmnt-len(cmt_sgn)]+cmt_sgn+line[pos_cmnt:             ]
-                    else:
-                        line = line[:pos_cmnt             ]+cmt_sgn+line[pos_cmnt+len(cmt_sgn):]
-                   #line = line[:pos_cmnt-len(cmt_sgn)]+cmt_sgn+line[pos_cmnt:]
-                   #line = line[:pos_body-len(cmt_sgn)]+cmt_sgn+line[pos_body:]
-               #elif cmt_type=='bod' and save_bd_col #  !line.startswith(blnks4cmt) :
-                elif cmt_type=='bod':#  !save_bd_col
-                    pos_cmnt = col_min_bd if at_min_bd else pos_body
-                    pass;      #LOG and log('pos_cmnt={}', (pos_cmnt))
-                    line = line[:pos_cmnt]             +cmt_sgn+line[pos_cmnt:]
-                   #line = line[:pos_body]             +cmt_sgn+line[pos_body:]
-
-            pass;              #LOG and log('new line={}', (line))
-            ed_.set_text_line(rWrk, line)
-            #for rWrk
-        bSkip    = get_opt('comment_move_down', True)
-        if bEmpSel and bSkip:
-            (cCrt, rCrt, cEnd, rEnd)    = crts[0]
-            _move_caret_down(cCrt, rCrt)
-       #def _cmt_toggle_line
-
-    def cmt_toggle_stream(self):
-        ''' '''
-        if ed.get_sel_mode() != app.SEL_NORMAL:
-            return app.msg_status(ONLY_NORM_SEL_MODE.format(COMMENTING))
-        lex     = ed.get_prop(app.PROP_LEXER_CARET)
-        ((bgn_sgn
-        ,end_sgn)
-        ,bOnlyLn)=self._get_cmt_pair(lex)
-        if not bgn_sgn:
-            return app.msg_status(CMT_NO_STRM_4LEX.format(lex))
-        bUseFLn = get_opt('comment_full_line_if_no_sel', True)
-        crts    = ed.get_carets()
-        pass;                  #LOG and log('lex, get_carets()={}', (lex, crts))
-        pass;                  #LOG and log('(bgn_sgn,end_sgn),bOnlyLn,bUseFLn={}', ((bgn_sgn,end_sgn),bOnlyLn,bUseFLn))
-        for icrt, (cCrt, rCrt, cEnd, rEnd) in enumerate(crts):
-            pass;              #LOG and log('(cCrt, rCrt), (cEnd, rEnd)={}', ((cCrt, rCrt), (cEnd, rEnd)))
-            bEmpSel     = -1==rEnd
-            bDrtSel     = -1==rEnd or (rCrt, cCrt)>(rEnd, cEnd)
-            if False:pass
-            elif bEmpSel and (bUseFLn or bOnlyLn):
-                # Use full line
-                line        = ed.get_text_line(rCrt)
-                (cTx1, rTx1), (cTx2, rTx2) = (0, rCrt), (len(line), rCrt)
-            elif bOnlyLn: # and not bEmpSel
-                # Only full lines
-                rTx1, rTx2  = minmax(rCrt, rEnd)
-                line    = ed.get_text_line(rTx2)
-                (cTx1, rTx1), (cTx2, rTx2) = (0, rTx1), (len(line), rTx2)
-            elif bEmpSel: # and not bUseFLn and not bOnlyLn
-                continue
-            else:
-                (rTx1, cTx1), (rTx2, cTx2) = minmax((rCrt, cCrt), (rEnd, cEnd))
-            selTx   = ed.get_text_substr(cTx1, rTx1, cTx2, rTx2)
-            pass;              #LOG and log('(rTx1, cTx1), (rTx2, cTx2), selTx={}', ((rTx1, cTx1), (rTx2, cTx2), repr(selTx)))
-            do_uncmt= selTx.startswith(bgn_sgn) and selTx.endswith(end_sgn)
-            pass;              #LOG and log('do_uncmt={}', (do_uncmt))
-
-            if False:pass
-            elif not do_uncmt and bOnlyLn:
-                # Comment!
-                ed.insert(0, rTx2+1, end_sgn+'\n')    #! true insert sequence
-                ed.insert(0, rTx1,   bgn_sgn+'\n')    #! true insert sequence
-                (cNSel1, rNSel1
-                ,cNSel2, rNSel2)    = 0, rTx1, len(end_sgn), rTx2+2
-            elif not do_uncmt:
-                # Comment!
-                ed.insert(cTx2, rTx2, end_sgn)        #! true insert sequence
-                ed.insert(cTx1, rTx1, bgn_sgn)        #! true insert sequence
-                if False:pass
-                elif rTx1==rTx2:
-                    # sel into one row
-                    (cNSel1, rNSel1
-                    ,cNSel2, rNSel2)    = cTx1, rTx1, cTx2+len(bgn_sgn)+len(end_sgn), rTx2
-                elif rTx1!=rTx2:
-                    # sel ends on diff rows
-                    (cNSel1, rNSel1
-                    ,cNSel2, rNSel2)    = cTx1, rTx1, cTx2             +len(end_sgn), rTx2
-
-            elif do_uncmt and bOnlyLn:
-                # UnComment!
-                ed.delete(0, rTx2, 0, rTx2+1)    #! true delete sequence
-                ed.delete(0, rTx1, 0, rTx1+1)    #! true delete sequence
-                (cNSel1, rNSel1
-                ,cNSel2, rNSel2)    = 0, rTx1, len(ed.get_text_line(rTx2-2)), rTx2-2
-            elif do_uncmt:
-                # UnComment!
-                ed.delete(cTx2-len(end_sgn), rTx2, cTx2, rTx2)    #! true delete sequence
-                ed.delete(cTx1, rTx1, cTx1+len(bgn_sgn), rTx1)    #! true delete sequence
-                if False:pass
-                elif rTx1==rTx2:
-                    # sel into one row
-                    (cNSel1, rNSel1
-                    ,cNSel2, rNSel2)    = cTx1, rTx1, cTx2-len(bgn_sgn)-len(end_sgn), rTx2
-                elif rTx1!=rTx2:
-                    # sel ends on diff rows
-                    (cNSel1, rNSel1
-                    ,cNSel2, rNSel2)    = cTx1, rTx1, cTx2             -len(end_sgn), rTx2
-
-            pass;              #LOG and log('bDrtSel, (cNSel1, rNSel1), (cNSel2, rNSel2)={}', (bDrtSel, (cNSel1, rNSel1), (cNSel2, rNSel2)))
-            if bDrtSel:
-                ed.set_caret(cNSel2, rNSel2, cNSel1, rNSel1, app.CARET_SET_INDEX+icrt)
-            else:
-                ed.set_caret(cNSel1, rNSel1, cNSel2, rNSel2, app.CARET_SET_INDEX+icrt)
-           #for icrt
-        bSkip    = get_opt('comment_move_down', True)
-        if False:pass
-        elif 1==len(crts) and bEmpSel and bUseFLn and bSkip:
-            _move_caret_down(cCrt, rCrt)
-            if bOnlyLn and not do_uncmt:
-                crt=ed.get_carets()[0]; _move_caret_down(crt[0], crt[1])
-                crt=ed.get_carets()[0]; _move_caret_down(crt[0], crt[1])
-        #def cmt_toggle_stream
-
-    def _get_cmt_pair(self, lex):
-        ''' Return ((begin_sign, end_sign), only_lines)
-                begin_sign    as '/*'
-                end_sign      as '*/'
-                only_lines    True if each of *_sign must be whole line
-        '''
-        if lex not in self.pair4lex:
-            # Search lex-pair
-            def_lexs_json   = os.path.join(get_def_setting_dir()             , 'default_lexers.json')
-            usr_lexs_json   = os.path.join(app.app_path(app.APP_DIR_SETTINGS), 'user_lexers.json')
-            def_lexs        = _json_loads(open(def_lexs_json, encoding='utf8').read())
-            usr_lexs        = _json_loads(open(usr_lexs_json, encoding='utf8').read()) if os.path.exists(usr_lexs_json) else {"Comments":{}, "CommentsForLines":{}}
-            only_ln        = False
-            if False:pass
-            elif lex in   usr_lexs["Comments"]:
-                pair    = usr_lexs["Comments"].get(lex)
-            elif lex in   usr_lexs["CommentsForLines"]:
-                pair    = usr_lexs["CommentsForLines"].get(lex)
-                only_ln = True
-            elif lex in   def_lexs["Comments"]:
-                pair    = def_lexs["Comments"].get(lex)
-            elif lex in   def_lexs["CommentsForLines"]:
-                pair    = def_lexs["CommentsForLines"].get(lex)
-                only_ln = True
-            else:
-                pair    = ['','']
-            self.pair4lex[lex] = (pair, only_ln)
-        return self.pair4lex[lex]
-       #def _get_cmt_pair
-
     #################################################
     ## Duplicate
     def duplicate(self):
@@ -322,12 +79,7 @@ class Command:
         ed.insert(cFr, rFr, sel_txt)
        #def duplicate
 
-    def __init__(self):
-        self.opts     = {}
-        self.pair4lex = {}
-        #def __init__
-
-    #class Command:
+    #class Command
 
 #################################################
 ## Common APP utils
