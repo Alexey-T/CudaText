c3d46489d02a1994439c0015a3235d597364a4f1
Alexey-t <support@uvviewsoft.com>
2016-09-22 20:51:43 +0300

    rename sh-files

--- setup/lin.sh
+++ setup/lin.sh
@@ -1,9 +1,10 @@
-#! /bin/sh
-cd ~/cuda/cuda/app
+#!/bin/sh
+cd ../app
 
-name=~/Public/cudatext-linux-qt-amd64.tar
+mkdir -p ~/Public
+name=~/Public/cudatext-linux-gtk2-amd64.tar
 
-rm ~/Public/*qt*.xz
+rm ~/Public/*gtk*.xz
 tar --exclude=*.pyc -cf $name cudatext readme data settings_default py/*.py py/cuda_addonman py/cuda_make_plugin py/cuda_insert_time py/cudax_lib py/requests py/cuda_comments
 
 cd ../setup/debfiles
