7432823466dcfd1c51e36de7343e377a570d6f51
Alexey-t <support@uvviewsoft.com>
2016-10-27 15:55:33 +0300

    upd scripts

--- setup/deb.sh
+++ setup/deb.sh
@@ -23,6 +23,7 @@ mkdir $dd/usr/share/cudatext/py/cuda_addonman
 mkdir $dd/usr/share/cudatext/py/cuda_comments
 mkdir $dd/usr/share/cudatext/py/cuda_make_plugin
 mkdir $dd/usr/share/cudatext/py/cuda_insert_time
+mkdir $dd/usr/share/cudatext/py/cuda_new_file
 mkdir $dd/usr/share/cudatext/py/cudax_lib
 mkdir $dd/usr/share/cudatext/py/requests
 mkdir $dd/usr/share/pixmaps
@@ -45,6 +46,8 @@ cp -r ../app/py/cuda_make_plugin/*.py $dd/usr/share/cudatext/py/cuda_make_plugin
 cp -r ../app/py/cuda_make_plugin/*.inf $dd/usr/share/cudatext/py/cuda_make_plugin
 cp -r ../app/py/cuda_insert_time/*.py $dd/usr/share/cudatext/py/cuda_insert_time
 cp -r ../app/py/cuda_insert_time/*.inf $dd/usr/share/cudatext/py/cuda_insert_time
+cp -r ../app/py/cuda_new_file/*.py $dd/usr/share/cudatext/py/cuda_new_file
+cp -r ../app/py/cuda_new_file/*.inf $dd/usr/share/cudatext/py/cuda_new_file
 cp -r ../app/py/cudax_lib/*.py $dd/usr/share/cudatext/py/cudax_lib
 cp -r ../app/py/requests/* $dd/usr/share/cudatext/py/requests
 
--- setup/lin_qt.sh
+++ setup/lin_qt.sh
@@ -10,7 +10,7 @@ mkdir -p $outdir
 name=$outdir/cudatext-linux-$widget-$cpu-$cuda_ver.tar
 
 rm $outdir/*-$widget-*.xz
-tar --exclude=*.pyc -cf $name cudatext readme data settings_default py/*.py py/cuda_addonman py/cuda_make_plugin py/cuda_insert_time py/cudax_lib py/requests py/cuda_comments
+tar --exclude=*.pyc -cf $name cudatext readme data settings_default py/*.py py/cuda_addonman py/cuda_make_plugin py/cuda_insert_time py/cudax_lib py/requests py/cuda_comments py/cuda_new_file
 
 cd ../setup/debfiles
 tar -rf $name cudatext-256.png
