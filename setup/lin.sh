e58f937d580611761552aebbb0576c8fbfa1f581
Alexey-t <support@uvviewsoft.com>
2016-12-01 07:38:45 +0300

    update setup files

--- setup/lin.sh
+++ setup/lin.sh
@@ -1,18 +1,2 @@
 #!/bin/sh
-. ./cuda_ver.sh
-cd ../app
-
-widget=gtk2
-cpu=amd64
-
-outdir=~/Public
-mkdir -p $outdir
-name=$outdir/cudatext-linux-$widget-$cpu-$cuda_ver.tar
-
-rm $outdir/*-$widget-*.xz
-tar --exclude=*.pyc -cf $name cudatext readme data settings_default py/*.py py/cuda_addonman py/cuda_make_plugin py/cuda_insert_time py/cudax_lib py/requests py/cuda_comments py/cuda_new_file py/cuda_palette
-
-cd ../setup/debfiles
-tar -rf $name cudatext-256.png
-
-xz -z $name
+./lin_common.sh gtk2
