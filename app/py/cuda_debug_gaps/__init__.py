bfe7e39a2bdc9b9415555a4e7d6fa46dc6d8304a
Alexey-t <support@uvviewsoft.com>
2016-12-25 09:02:47 +0300

    cuda_debug_gaps test

---
+++ app/py/cuda_debug_gaps/__init__.py
@@ -0,0 +1,15 @@
+from cudatext import *
+
+class Command:
+    def run(self):
+        for i in range(ed.get_line_count()//2):
+            self.do_gap(i*2)
+
+    def do_gap(self, num):
+        id_bitmap, id_canvas = ed.gap(GAP_MAKE_BITMAP, 600, 50)
+        canvas_proc(id_canvas, CANVAS_SET_BRUSH, color=0xa0ffa0)
+        canvas_proc(id_canvas, CANVAS_POLYGON, '200,0,300,30,200,50')
+        canvas_proc(id_canvas, CANVAS_SET_BRUSH, color=0xffffff, style=BRUSH_CLEAR)
+        canvas_proc(id_canvas, CANVAS_TEXT, x=230, y=10, text='gap %d'%(num+1))
+        canvas_proc(id_canvas, CANVAS_SET_BRUSH, color=0xffffff, style=BRUSH_SOLID)
+        ed.gap(GAP_ADD, num, id_bitmap)
