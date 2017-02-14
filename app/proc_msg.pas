23adaf2fe398ca245af2be054b2e8c8676490a7c
Alexey-t <support@uvviewsoft.com>
2017-02-14 17:14:34 +0300

    i18n

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -89,7 +89,7 @@ const
   msgCannotOpenTooBig: string = 'Cannot open file, it''s too big:';
   msgCannotHandleZip: string = 'Cannot handle zip file:';
   msgCannotSetHotkey: string = 'Cannot set hotkey for this item';
-  msgCannotInstallAddonApi: string = 'Cannot install add-on "%s", it requires API version %s';
+  msgCannotInstallAddonApi: string = 'Cannot install add-on "%s", it needs newer application version (API %s)';
 
   msgStatusIncorrectFilename: string = 'Incorrect filename:';
   msgStatusIncorrectInstallInfInZip: string = 'Incorrect install.inf in zip';
