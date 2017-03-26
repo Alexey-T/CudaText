9588ef1aadcd2a06952ef0c955fc622103f1d016
Alexey-t <support@uvviewsoft.com>
2017-03-27 00:22:26 +0300

    fix #773

--- app/form_addon_report.pas
+++ app/form_addon_report.pas
@@ -40,6 +40,10 @@ begin
     repeat
       S:= SGetItem(SAll, #10);
       if S='' then Break;
+
+      S:= StringReplace(S, '&', '', [rfReplaceAll]);
+      S:= StringReplace(S, '\', ': ', [rfReplaceAll]);
+
       F.ListBox1.Items.Add(S);
     until false;
 
