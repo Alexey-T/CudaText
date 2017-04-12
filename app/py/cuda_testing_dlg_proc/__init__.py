7ab04a1a8ea25ef0ec83abe103ec4d6861d6273d
Alexey-t <support@uvviewsoft.com>
2017-04-12 21:42:20 +0300

    testing plugin, dlg_proc

---
+++ app/py/cuda_testing_dlg_proc/__init__.py
@@ -0,0 +1,39 @@
+from cudatext import *
+
+class Command:
+    def on_dlg(self, ed_self, id_dlg, id_ctl, id_event):
+        print(id_event)
+        dlg_proc(id_dlg, DLG_FREE)
+
+
+    def run(self):
+        h=dlg_proc(0, DLG_CREATE)
+        dlg_proc(h, DLG_PROP_SET, prop={'cap':'TestDlg', 'x':100, 'y':50, 'w':400, 'h':300})
+
+        n=dlg_proc(h, DLG_CTL_ADD, 'label')
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'cap':'label', 'x':10, 'y':10, 'w':50, 'act':True})
+
+        n=dlg_proc(h, DLG_CTL_ADD, 'check')
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'cap':'chk', 'x':60, 'y':10, 'w':200, 'act':True})
+
+        n=dlg_proc(h, DLG_CTL_ADD, 'edit')
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'val':'edit1', 'x':10, 'y':30, 'w':200} )
+
+        n=dlg_proc(h, DLG_CTL_ADD, 'button')
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'cap':'&Close', 'x':10, 'y':60, 'w':80} )
+
+        n=dlg_proc(h, DLG_CTL_ADD, 'button')
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'cap':'&Btn', 'x':100, 'y':60, 'w':200} )
+
+        dlg_proc(h, DLG_CTL_FOCUS, index=3)
+        dlg_proc(h, DLG_SHOW_NONMODAL)
+        return
+
+        res = dlg_proc(h, DLG_PROP_GET)
+        print('dlg_proc:', res)
+
+        cnt = dlg_proc(h, DLG_CTL_COUNT)
+        for n in range(cnt):
+            res = dlg_proc(h, DLG_CTL_PROP_GET, index=n)
+            print('ctl', n, ':', res)
+        dlg_proc(h, DLG_FREE)
