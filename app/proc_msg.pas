c6eb60d61b9ba885f8f3ccb9c229f0f36f67510b
Alexey-t <support@uvviewsoft.com>
2017-04-18 16:55:06 +0300

    txt

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -15,8 +15,8 @@ uses
   Classes, SysUtils;
 
 const
-  cAppExeVersion = '1.8.1.0';
-  cAppApiVersion = '1.0.175';
+  cAppExeVersion = '1.8.2.0';
+  cAppApiVersion = '1.0.176';
 
 const
   msgTitle = 'CudaText'; //no need i18n
