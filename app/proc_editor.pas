fc868b6bd3fa638eb6e397a671592e60fc620187
Alexey-t <support@uvviewsoft.com>
2017-04-20 10:34:31 +0300

    comment

--- app/proc_editor.pas
+++ app/proc_editor.pas
@@ -174,7 +174,10 @@ begin
   Ed.Font.Quality:= Op.OpFontQuality;
   Ed.OptShowFontLigatures:= Op.OpFontLigatures;
 
+  {$ifdef windows}
+  //no effect on gtk2, not tested on Mac
   Ed.OptCharSpacingX:= Op.OpSpacingX;
+  {$endif}
   Ed.OptCharSpacingY:= Op.OpSpacingY;
 
   Ed.OptTabSize:= Op.OpTabSize;
