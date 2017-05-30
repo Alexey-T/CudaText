08bf28d0e81bb2c8f3c3259205ccc2b24f8513f4
BkUser <hhhhhhhhnn70@gmail.com>
2017-05-30 10:24:43 +0300

    preinstall ProjManager

---
+++ app/py/cuda_project_man/dlg.py
@@ -0,0 +1,82 @@
+import os
+from cudatext import *
+
+DEFAULT_MASKS_IGNORE = '*.zip *.7z *.tar *.gz *.rar *.exe *.dll .git .svn'
+
+def dialog_config(op):
+
+    id_ignore = 1
+    id_recents = 3
+    id_on_start = 4
+    id_ok = 5
+
+    c1 = chr(1)
+    text = '\n'.join([]
+        +[c1.join(['type=label', 'pos=6,6,500,0', 'cap=&File/folder masks to ignore (space-separated):'])]
+        +[c1.join(['type=edit', 'pos=6,24,500,0',
+            'val='+op.get('masks_ignore', DEFAULT_MASKS_IGNORE)])]
+        +[c1.join(['type=label', 'pos=6,54,500,0', 'cap=&Recent projects:'])]
+        +[c1.join(['type=memo', 'pos=6,74,500,180',
+            'val='+'\t'.join(op.get('recent_projects', [])) ])]
+        +[c1.join(['type=check', 'pos=6,186,400,0', 'cap=&Load on app start',
+            'val='+('1' if op.get('on_start', False) else '0') ])]
+        +[c1.join(['type=button', 'pos=300,230,400,0', 'cap=&OK', 'props=1'])]
+        +[c1.join(['type=button', 'pos=406,230,502,0', 'cap=Cancel'])]
+    )
+
+    res = dlg_custom('Project Manager options', 508, 260, text)
+    if res is None:
+        return
+
+    res, text = res
+    text = text.splitlines()
+
+    if res != id_ok:
+        return
+
+    s = text[id_ignore].strip()
+    while '  ' in s:
+        s = s.replace('  ', ' ')
+    op['masks_ignore'] = s
+
+    s = text[id_recents].split('\t')
+    op['recent_projects'] = s
+
+    op['on_start'] = text[id_on_start]=='1'
+
+    return True
+
+
+def dialog_proj_prop(prop):
+
+    list_vars = prop.get('vars', '')
+    main_file = prop.get('mainfile', '')
+
+    id_vars = 1
+    id_ok = 4
+
+    c1 = chr(1)
+    text = '\n'.join([]
+        +[c1.join(['type=label', 'pos=6,6,500,0', 'cap=&Variables (in form Name=Value)'])]
+        +[c1.join(['type=memo', 'pos=6,26,500,180', 'val='+'\t'.join(list_vars) ])]
+        +[c1.join(['type=label', 'pos=6,186,500,0', 'cap=&Main file (read-only, change in context menu)'])]
+        +[c1.join(['type=edit', 'pos=6,206,500,0', 'props=1,0,1', 'val='+main_file])]
+        +[c1.join(['type=button', 'pos=300,300,400,0', 'cap=&OK', 'props=1'])]
+        +[c1.join(['type=button', 'pos=406,300,502,0', 'cap=Cancel'])]
+    )
+
+    res = dlg_custom('Project properties', 508, 330, text)
+    if res is None:
+        return
+
+    res, text = res
+    text = text.splitlines()
+
+    if res != id_ok:
+        return
+
+    s = text[id_vars].split('\t')
+    s = [item.strip() for item in s if '=' in item]
+    prop['vars'] = s
+
+    return True
---
+++ app/py/cuda_project_man/readme/readme.txt
@@ -0,0 +1,21 @@
+Plugin for CudaText.
+Allows to manage "projects" (files have .cuda-proj extension).
+Plugin shows "Project" panel in the side panel, with context menu.
+
+Context menu has commands:
+ - Add directory...
+ - Add file...
+ - New project
+ - Open project...
+ - Recent projects
+ - Save project as...
+ - Refresh
+ - Remove node
+ - Clear project
+
+Tech notes:
+- You can add only root nodes to tree-view. Project files store only links for root-nodes (files and dirs). File-list of folders is not saved to project. So project files are small.
+- When you call "Remove node", node on top project level is removed, and selection inside that node is ignored.
+- You can open project files from Open dialog, or command line. Instead of opening raw JSON content, project will be loaded.
+
+Homepage: https://github.com/pohmelie/cuda_project_man
