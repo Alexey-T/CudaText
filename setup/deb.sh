20793aac300971368380113c58f1e68d3c153e6a
BkUser <hhhhhhhhnn70@gmail.com>
2017-05-30 10:39:25 +0300

    fix deb setup

--- setup/deb.sh
+++ setup/deb.sh
@@ -50,9 +50,9 @@ cp -r ../app/py/cuda_insert_time/*.py $dd/usr/share/cudatext/py/cuda_insert_time
 cp -r ../app/py/cuda_insert_time/*.inf $dd/usr/share/cudatext/py/cuda_insert_time
 cp -r ../app/py/cuda_new_file/*.py $dd/usr/share/cudatext/py/cuda_new_file
 cp -r ../app/py/cuda_new_file/*.inf $dd/usr/share/cudatext/py/cuda_new_file
-cp -r ../app/py/cuda_project_man/*.py $dd/usr/share/cudatext/py/cuda_project_man
-cp -r ../app/py/cuda_project_man/*.inf $dd/usr/share/cudatext/py/cuda_project_man
 cp -r ../app/py/cuda_palette/* $dd/usr/share/cudatext/py/cuda_palette
+rm -rf ../app/py/cuda_project_man/__pycache__
+cp -r ../app/py/cuda_project_man $dd/usr/share/cudatext/py
 cp -r ../app/py/cudax_lib/*.py $dd/usr/share/cudatext/py/cudax_lib
 cp -r ../app/py/requests/* $dd/usr/share/cudatext/py/requests
 
