7b4ba57f9af3b78750e5ed41505911f60f0953a8
Alexey-t <support@uvviewsoft.com>
2017-06-05 20:33:28 +0300

    fix cuda_testing* plugins

--- app/py/cuda_testing_gaps/__init__.py
+++ app/py/cuda_testing_gaps/__init__.py
@@ -8,7 +8,8 @@ class Command:
     def do_gap(self, num):
         id_bitmap, id_canvas = ed.gap(GAP_MAKE_BITMAP, 600, 50)
         canvas_proc(id_canvas, CANVAS_SET_BRUSH, color=0xa0ffa0)
-        canvas_proc(id_canvas, CANVAS_POLYGON, '200,0,300,30,200,50')
+        canvas_proc(id_canvas, CANVAS_SET_ANTIALIAS, style=ANTIALIAS_ON)
+        canvas_proc(id_canvas, CANVAS_POLYGON, '200,0,300,30,200,49')
         canvas_proc(id_canvas, CANVAS_SET_BRUSH, color=0xffffff, style=BRUSH_CLEAR)
         canvas_proc(id_canvas, CANVAS_TEXT, x=230, y=10, text='gap %d'%(num+1))
         canvas_proc(id_canvas, CANVAS_SET_BRUSH, color=0xffffff, style=BRUSH_SOLID)
