05cc33def3421e5b2f1037b8257011e1a312639b
Alexey-t <support@uvviewsoft.com>
2017-06-09 02:07:22 +0300

    dlg_proc: adapt testing plugin

--- app/py/cuda_testing_dlg_proc/__init__.py
+++ app/py/cuda_testing_dlg_proc/__init__.py
@@ -2,11 +2,11 @@ import os
 from cudatext import *
 
 
-def callback_main_close(id_dlg, id_ctl, id_event='', info=''):
+def callback_main_close(id_dlg, id_ctl, info=''):
     print('callback_main_close')
     dlg_proc(id_dlg, DLG_HIDE)
 
-def callback_main_menu(id_dlg, id_ctl, id_event='', info=''):
+def callback_main_menu(id_dlg, id_ctl, info=''):
     print('callback_main_menu')
     nctl = dlg_proc(id_dlg, DLG_CTL_FIND, prop='btn_menu')
     d = dlg_proc(id_dlg, DLG_CTL_PROP_GET, index=nctl)
@@ -38,8 +38,8 @@ class Command:
     def run_dlgcustom(self):
         dlg_custom('TestDlg', 200, 100, 'type=label\1pos=6,6,200,0\1cap=Test')
 
-    def callback_maindlg(self, id_dlg, id_ctl, id_event='', info=''):
-        print('callback_maindlg(id_event="%s", info=%s)' % (id_event, repr(info)))
+    def callback_maindlg(self, id_dlg, id_ctl, info=''):
+        print('callback_maindlg(info=%s)' % repr(info))
         h = id_dlg
 
         n_chk = dlg_proc(h, DLG_CTL_FIND, prop='chk1')
@@ -51,90 +51,86 @@ class Command:
         n_chk_dock = dlg_proc(h, DLG_CTL_FIND, prop='chk_dock')
         n_paint = dlg_proc(h, DLG_CTL_FIND, prop='paint')
 
-        if id_event=='on_change':
-            if id_ctl==n_chk:
-                d = dlg_proc(h, DLG_CTL_PROP_GET, index=n_color)
-                dlg_proc(h, DLG_CTL_PROP_SET, index=n_color, prop={'vis': not d['vis']} )
+        if id_ctl==n_chk:
+            d = dlg_proc(h, DLG_CTL_PROP_GET, index=n_color)
+            dlg_proc(h, DLG_CTL_PROP_SET, index=n_color, prop={'vis': not d['vis']} )
 
-            if id_ctl==n_btn1:
-                d = dlg_proc(h, DLG_CTL_PROP_GET, index=n_edit)
-                dlg_proc(h, DLG_PROP_SET, prop={'cap': 'entered: '+d['val'] } )
+        if id_ctl==n_btn1:
+            d = dlg_proc(h, DLG_CTL_PROP_GET, index=n_edit)
+            dlg_proc(h, DLG_PROP_SET, prop={'cap': 'entered: '+d['val'] } )
 
-            if id_ctl==n_btn_dlg:
-                hh = self.init_tempdlg()
-                docked = dlg_proc(h, DLG_CTL_PROP_GET, index=n_chk_dock)['val'] == '1'
-                if docked:
-                    dlg_proc(hh, DLG_DOCK, prop='R', index=h)
-                    dlg_proc(hh, DLG_SHOW_NONMODAL)
-                else:
-                    dlg_proc(hh, DLG_SHOW_MODAL)
+        if id_ctl==n_btn_dlg:
+            hh = self.init_tempdlg()
+            docked = dlg_proc(h, DLG_CTL_PROP_GET, index=n_chk_dock)['val'] == '1'
+            if docked:
+                dlg_proc(hh, DLG_DOCK, prop='R', index=h)
+                dlg_proc(hh, DLG_SHOW_NONMODAL)
+            else:
+                dlg_proc(hh, DLG_SHOW_MODAL)
 
-            if id_ctl==n_btn_paint:
-                canvas_id = dlg_proc(h, DLG_CTL_HANDLE, index=n_paint)
-                canvas_proc(canvas_id, CANVAS_SET_PEN, color=0xA0)
-                canvas_proc(canvas_id, CANVAS_SET_BRUSH, color=0xA0FF00)
-                canvas_proc(canvas_id, CANVAS_RECT, x=0, y=0, x2=50, y2=50)
-                canvas_proc(canvas_id, CANVAS_SET_BRUSH, color=0xA0A0)
-                canvas_proc(canvas_id, CANVAS_ELLIPSE, x=0, y=0, x2=50, y2=50)
-
-        #if id_event=='on_resize':
-        #    d = dlg_proc(h, DLG_PROP_GET)
-        #    dlg_proc(h, DLG_CTL_PROP_SET, index=n_color, prop={'x': d['w']-20, 'h': d['h']-10 } )
+        if id_ctl==n_btn_paint:
+            canvas_id = dlg_proc(h, DLG_CTL_HANDLE, index=n_paint)
+            canvas_proc(canvas_id, CANVAS_SET_PEN, color=0xA0)
+            canvas_proc(canvas_id, CANVAS_SET_BRUSH, color=0xA0FF00)
+            canvas_proc(canvas_id, CANVAS_RECT, x=0, y=0, x2=50, y2=50)
+            canvas_proc(canvas_id, CANVAS_SET_BRUSH, color=0xA0A0)
+            canvas_proc(canvas_id, CANVAS_ELLIPSE, x=0, y=0, x2=50, y2=50)
 
 
-    def callback_tempdlg(self, id_dlg, id_ctl, id_event='', info=''):
-        print('callback_tempdlg', id_event)
+    def callback_tempdlg_on_key_down(self, id_dlg, id_ctl, info=''):
+        print('callback_tempdlg_on_key_down')
+
+        state = app_proc(PROC_GET_KEYSTATE, '')
+        str_key =\
+            ('Meta+' if 'm' in state else '')+\
+            ('Ctrl+' if 'c' in state else '')+\
+            ('Alt+' if 'a' in state else '')+\
+            ('Shift+' if 's' in state else '')+\
+            app_proc(PROC_HOTKEY_INT_TO_STR, str(id_ctl))
+
+        n_info = dlg_proc(id_dlg, DLG_CTL_FIND, 'label_info')
+        dlg_proc(id_dlg, DLG_CTL_PROP_SET, index=n_info, prop={'cap': 'keypress: '+str_key })
+        return True
+
+    def callback_tempdlg_on_close_query(self, id_dlg, id_ctl, info=''):
+        print('callback_tempdlg_on_close_query')
+        n_canclose = dlg_proc(id_dlg, DLG_CTL_FIND, 'chk_canclose')
+        d = dlg_proc(id_dlg, DLG_CTL_PROP_GET, index=n_canclose)
+        return d['val']=='1'
+
+    def callback_tempdlg(self, id_dlg, id_ctl, info=''):
+        print('callback_tempdlg')
 
         n_close = dlg_proc(id_dlg, DLG_CTL_FIND, 'btn_close')
         n_clone = dlg_proc(id_dlg, DLG_CTL_FIND, 'btn_clonedlg')
-        n_canclose = dlg_proc(id_dlg, DLG_CTL_FIND, 'chk_canclose')
-        n_info = dlg_proc(id_dlg, DLG_CTL_FIND, 'label_info')
 
-        if id_event=='on_key_down':
-            state = app_proc(PROC_GET_KEYSTATE, '')
-            str_key =\
-                ('Meta+' if 'm' in state else '')+\
-                ('Ctrl+' if 'c' in state else '')+\
-                ('Alt+' if 'a' in state else '')+\
-                ('Shift+' if 's' in state else '')+\
-                app_proc(PROC_HOTKEY_INT_TO_STR, str(id_ctl))
-            dlg_proc(id_dlg, DLG_CTL_PROP_SET, index=n_info, prop={'cap': 'keypress: '+str_key })
-            return True
+        if id_ctl==n_close:
+            dlg_proc(id_dlg, DLG_HIDE)
 
-        if id_event=='on_close_query':
-            d = dlg_proc(id_dlg, DLG_CTL_PROP_GET, index=n_canclose)
-            #print('canclose:', d['val'])
-            return d['val']=='1'
-
-        if id_event=='on_change':
-            if id_ctl==n_close:
-                dlg_proc(id_dlg, DLG_HIDE)
-
-            if id_ctl==n_clone:
-                print('  tempdlg begin')
-                d = dlg_proc(id_dlg, DLG_PROP_GET)
-                hh = self.init_tempdlg(d['x']+20, d['y']+20)
-                dlg_proc(hh, DLG_SHOW_MODAL)
-                dlg_proc(hh, DLG_FREE)
-                print('  tempdlg end')
-
+        if id_ctl==n_clone:
+            print('  tempdlg begin')
+            d = dlg_proc(id_dlg, DLG_PROP_GET)
+            hh = self.init_tempdlg(d['x']+20, d['y']+20)
+            dlg_proc(hh, DLG_SHOW_MODAL)
+            dlg_proc(hh, DLG_FREE)
+            print('  tempdlg end')
 
 
     def init_maindlg(self):
         h=dlg_proc(0, DLG_CREATE)
-        dlg_proc(h, DLG_PROP_SET, prop={'cap':'main dlg', 'x':100, 'y':50, 'w':400, 'h':300, 'resize':True, 'w_min': 200, 'h_min': 100, 'topmost':True, 'callback': 'cuda_testing_dlg_proc.callback_maindlg', 'events': '*' })
+        dlg_proc(h, DLG_PROP_SET, prop={'cap':'main dlg', 'x':100, 'y':50, 'w':400, 'h':300, 'resize':True, 'w_min': 200, 'h_min': 100, 'topmost':True })
 
         n=dlg_proc(h, DLG_CTL_ADD, 'label')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': '', 'cap':'label', 'x':10, 'y':10, 'w':50, 'tag': 'some_tag' })
 
         n=dlg_proc(h, DLG_CTL_ADD, 'check')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk1', 'cap':'show panel', 'val':True, 'x':60, 'y':8, 'w':200, 'act':True })
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk1', 'cap':'show panel', 'val':True, 'x':60, 'y':8, 'w':200, 'act':True, 'callback': 'cuda_testing_dlg_proc.callback_maindlg' })
 
         n=dlg_proc(h, DLG_CTL_ADD, 'edit')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'edit1', 'val':'edit1', 'x':10, 'y':30, 'w':200} )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn1', 'cap':'Btn&1: caption', 'x':10, 'y':60, 'w':100} )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn1', 'cap':'Btn&1: caption', 'x':10, 'y':60, 'w':100, 'callback': 'cuda_testing_dlg_proc.callback_maindlg'} )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn2', 'cap':'Btn&2: move', 'x':120, 'y':60, 'w':100, 'callback': 'cuda_testing_dlg_proc.callback_main_movebtn'} )
@@ -145,16 +141,16 @@ class Command:
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={ 'a_l': None, 'a_r': ('', ']'), 'a_b': ('', ']'), 'sp_a': 6  } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_x_panel', 'cap':'x', 'x':0, 'y':0, 'w':15, 'h':15, 'p': 'color', 'a_t': ('color', '-') } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_x_panel', 'cap':'x', 'x':0, 'y':0, 'w':15, 'h':15, 'p': 'color', 'a_t': ('color', '-'), 'callback': 'cuda_testing_dlg_proc.callback_maindlg' } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_dlg', 'cap':'temp dlg', 'x':10, 'y':200, 'w':100} )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_dlg', 'cap':'temp dlg', 'x':10, 'y':200, 'w':100, 'callback': 'cuda_testing_dlg_proc.callback_maindlg'} )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_menu', 'cap':'menu here', 'x':10, 'y':230, 'w':100, 'callback': 'module=cuda_testing_dlg_proc;func=callback_main_menu;'} )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_paint', 'cap':'paint here', 'x':10, 'y':260, 'w':100 } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_paint', 'cap':'paint here', 'x':10, 'y':260, 'w':100, 'callback': 'cuda_testing_dlg_proc.callback_maindlg' } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_callbk', 'cap':'complex callback', 'x':120, 'y':200, 'w':120, 'callback': 'module=cuda_testing_dlg_proc.testcall;func=callback_main_complex;info=1234;'} )
@@ -163,7 +159,7 @@ class Command:
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_ok', 'cap':'close', 'x':120, 'y':230, 'w':120, 'callback': 'module=cuda_testing_dlg_proc;func=callback_main_close;'} )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'check')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk_dock', 'cap':'temp dlg: docked', 'x':10, 'y':170, 'w':120 } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk_dock', 'cap':'temp dlg: docked', 'x':10, 'y':170, 'w':120, 'callback': 'cuda_testing_dlg_proc.callback_maindlg' } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'paintbox')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'paint', 'x':250, 'y':200, 'w':60, 'h':60 } )
@@ -185,13 +181,17 @@ class Command:
 
     def init_tempdlg(self, x=150, y=150):
         h=dlg_proc(0, DLG_CREATE)
-        dlg_proc(h, DLG_PROP_SET, prop={'cap':'temp dlg', 'x':x, 'y':y, 'w':300, 'h':200, 'callback': 'cuda_testing_dlg_proc.callback_tempdlg', 'events': '*', 'color': 0xc0c0c0 })
+        dlg_proc(h, DLG_PROP_SET, prop={'cap':'temp dlg', 'x':x, 'y':y, 'w':300, 'h':200,
+            'color': 0xc0c0c0,
+            'callback_on_key_down': 'cuda_testing_dlg_proc.callback_tempdlg_on_key_down',
+            'callback_on_close_query': 'cuda_testing_dlg_proc.callback_tempdlg_on_close_query',
+            })
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_close', 'cap':'close', 'x':20, 'y':20, 'w':100 })
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_close', 'cap':'close', 'x':20, 'y':20, 'w':100, 'callback': 'cuda_testing_dlg_proc.callback_tempdlg' })
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_clonedlg', 'cap':'clone dlg', 'x':20, 'y':50, 'w':100 })
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_clonedlg', 'cap':'clone dlg', 'x':20, 'y':50, 'w':100, 'callback': 'cuda_testing_dlg_proc.callback_tempdlg' })
 
         n=dlg_proc(h, DLG_CTL_ADD, 'check')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk_canclose', 'cap':'can close form', 'x':20, 'y':80, 'w':100, 'val':True })
@@ -211,7 +211,7 @@ class Command:
         return h
 
 
-    def callback_main_movebtn(self, id_dlg, id_ctl, id_event=''):
+    def callback_main_movebtn(self, id_dlg, id_ctl):
         print('callback_main_movebtn')
         d = dlg_proc(id_dlg, DLG_CTL_PROP_GET, index=id_ctl)
         dlg_proc(id_dlg, DLG_CTL_PROP_SET, index=id_ctl, prop={'x': d['x']+10, 'y': d['y']+8 } )
@@ -243,16 +243,16 @@ class Command:
         icon2 = os.path.join(dir_icons, 'find.png')
         print('icon1:', icon1)
         print('icon2:', icon2)
-    
+
         id = dlg_proc(0, DLG_CREATE)
 
         dlg_proc(id, DLG_PROP_SET, {'w':400, 'h':300, 'cap':'Test type=toolbar', 'resize':True})
 
         n = dlg_proc(id, DLG_CTL_ADD, 'toolbar')
         dlg_proc(id, DLG_CTL_PROP_SET, index=n, prop={'name': 'tb', 'x':0, 'y':0, 'w':20, 'h':40, 'a_r': ('', ']'), 'color': 0x80B080 })
-        
+
         tb_id = dlg_proc(id, DLG_CTL_HANDLE, index=n)
-        
+
         toolbar_proc(tb_id, TOOLBAR_SET_ICON_SIZES, index=24, index2=24)
         n1 = toolbar_proc(tb_id, TOOLBAR_ADD_ICON, text=icon1)
         n2 = toolbar_proc(tb_id, TOOLBAR_ADD_ICON, text=icon2)
@@ -262,4 +262,4 @@ class Command:
 
         dlg_proc(id, DLG_SHOW_MODAL)
         dlg_proc(id, DLG_FREE)
-        
+
--- app/py/cuda_testing_dlg_proc/testcall.py
+++ app/py/cuda_testing_dlg_proc/testcall.py
@@ -1,2 +1,2 @@
-def callback_main_complex(id_dlg, id_ctl, ed_event='', info=''):
+def callback_main_complex(id_dlg, id_ctl, info=''):
     print('callback_main_complex(info=%s)' % info)
