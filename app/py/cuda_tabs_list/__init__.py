1e83c2ab3ec94f823c3a699189e5b5390861b15e
Alexey-t <support@uvviewsoft.com>
2017-06-08 14:14:33 +0300

    TabsList: added config command

--- app/py/cuda_tabs_list/__init__.py
+++ app/py/cuda_tabs_list/__init__.py
@@ -1,13 +1,17 @@
+import os
 from cudatext import *
 import cudatext_cmd
 
-option_open_at_start = False
-
+fn_config = 'cuda_tabs_list.ini'
 
 class Command:
     title = 'Tabs'
     h_tree = None
     busy_update = False
+    open_on_start = False
+
+    def __init__(self):
+        self.open_on_start = ini_read(fn_config, 'op', 'on_start', '0')=='1'
 
     def open(self, activate_tab=True):
         ed.cmd(cudatext_cmd.cmd_ShowSidePanelAsIs)
@@ -71,7 +75,7 @@ class Command:
             self.update()
 
     def on_start(self, ed_self):
-        if option_open_at_start:
+        if self.open_on_start:
             self.open(False)
 
     def ed_of_sel(self):
@@ -98,3 +102,18 @@ class Command:
     def menu_copy_file_name(self):
         e = self.ed_of_sel()
         e.cmd(cudatext_cmd.cmd_CopyFilenameName)
+
+
+    def config(self):
+        text = '\n'.join([
+            'type=check\1pos=6,6,500,0\1cap=Show Tabs panel on application start\1val='+('1' if self.open_on_start else '0'),
+            'type=button\1pos=324,70,404,0\1cap=OK\1props=1',
+            'type=button\1pos=410,70,490,0\1cap=Cancel',
+            ])
+
+        res = dlg_custom('Tabs List options', 500, 100, text, get_dict=True)
+        if res is None: return
+        if res['clicked'] != 1: return
+
+        self.open_on_start = res[0]=='1'
+        ini_write(fn_config, 'op', 'on_start', '1' if self.open_on_start else '0')
