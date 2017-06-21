e9cd5f71fb9d3b23ff41133c724cfef8a3018ffc
Alexey-t <support@uvviewsoft.com>
2017-06-21 14:33:38 +0300

    Projman small upd

--- app/py/cuda_project_man/projman_dlg.py
+++ app/py/cuda_project_man/projman_dlg.py
@@ -2,9 +2,9 @@ import os
 from cudatext import *
 
 MASKS_IGNORE = '.rar .exe .dll .git .svn'
-MASKS_ZIP = '.zip .7z .tar .gz .rar .xz .cab'
+MASKS_ZIP = '.zip .7z .tar .gz .rar .xz .cab .deb .rpm'
 MASKS_IMAGES = '.png .jpg .jpeg .gif .bmp .ico'
-MASKS_BINARY = '.exe .dll .o .msi .lib'
+MASKS_BINARY = '.exe .dll .o .msi .lib .obj .pdf'
 
 
 def dialog_config(op):
