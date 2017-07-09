899baed01599c0a79e02150fa22a78a71c6e1a39
BkUser <hhhhhhhhnn70@gmail.com>
2017-07-09 19:52:05 +0300

    ver

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -15,7 +15,7 @@ uses
   Classes, SysUtils;
 
 const
-  cAppExeVersion = '1.13.0.0';
+  cAppExeVersion = '1.13.1.0';
   cAppApiVersion = '1.0.189';
 
 const
--- setup/cuda_ver.sh
+++ setup/cuda_ver.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cuda_ver=1.13.0.0
+cuda_ver=1.13.1.0
