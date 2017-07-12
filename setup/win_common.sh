67e41cdc790dc265412a009d9684757ce899d9f5
BkUser <hhhhhhhhnn70@gmail.com>
2017-07-12 14:50:24 +0300

    fix win_commin.sh

--- setup/win_common.sh
+++ setup/win_common.sh
@@ -32,6 +32,7 @@ curl -L https://github.com/Alexey-T/ATGroups/archive/master.zip >ATGroups.zip
 curl -L https://github.com/Alexey-T/ATScrollBar/archive/master.zip >ATScrollBar.zip
 curl -L https://github.com/Alexey-T/ATStatus/archive/master.zip >ATStatus.zip
 curl -L https://github.com/Alexey-T/ATSynEdit/archive/master.zip >ATSynEdit.zip
+curl -L https://github.com/Alexey-T/ATSynEdit_Ex/archive/master.zip >ATSynEdit_Ex.zip
 curl -L https://github.com/Alexey-T/ATTabs/archive/master.zip >ATTabs.zip
 curl -L https://github.com/Alexey-T/EControl/archive/master.zip >EControl.zip
 curl -L https://github.com/Alexey-T/Python-for-Lazarus/archive/master.zip >Python.zip
