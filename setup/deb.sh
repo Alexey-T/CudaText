9e17c3cda72efed1c45680a4365a3c13676c6579
Alexey-t <support@uvviewsoft.com>
2017-09-06 12:36:35 +0300

    fix deb script

--- setup/deb.sh
+++ setup/deb.sh
@@ -27,6 +27,7 @@ mkdir $dd/usr/share/cudatext/py/cuda_new_file
 mkdir $dd/usr/share/cudatext/py/cuda_palette
 mkdir $dd/usr/share/cudatext/py/cuda_project_man
 mkdir $dd/usr/share/cudatext/py/cuda_tabs_list
+mkdir $dd/usr/share/cudatext/py/cuda_show_unsaved
 mkdir $dd/usr/share/cudatext/py/cudax_lib
 mkdir $dd/usr/share/cudatext/py/requests
 mkdir $dd/usr/share/pixmaps
@@ -56,6 +57,8 @@ rm -rf ../app/py/cuda_project_man/__pycache__
 cp -r ../app/py/cuda_project_man $dd/usr/share/cudatext/py
 rm -rf ../app/py/cuda_tabs_list/__pycache__
 cp -r ../app/py/cuda_tabs_list $dd/usr/share/cudatext/py
+rm -rf ../app/py/cuda_show_unsaved/__pycache__
+cp -r ../app/py/cuda_show_unsaved $dd/usr/share/cudatext/py
 cp -r ../app/py/cudax_lib/*.py $dd/usr/share/cudatext/py/cudax_lib
 cp -r ../app/py/requests/* $dd/usr/share/cudatext/py/requests
 
