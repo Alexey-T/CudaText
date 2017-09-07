fa565b4e7698a29d409105d4a1a9077998c20532
Alexey-t <support@uvviewsoft.com>
2017-09-07 10:13:56 +0300

    ShowUnsaved: added buttons Close/Save

--- app/py/cuda_show_unsaved/__init__.py
+++ app/py/cuda_show_unsaved/__init__.py
@@ -19,17 +19,19 @@ class Command:
             msg_box('File is not changed', MB_OK+MB_ICONINFO)
             return
 
-        text = '\n'.join(diff)+'\n'
-        h = self.init_editor_dlg(fn_base, text)
-        dlg_proc(h, DLG_SHOW_MODAL)
-        dlg_proc(h, DLG_FREE)
+        self.text = '\n'.join(diff)+'\n'
+        self.filename = fn_base
+        self.h_dlg = self.init_editor_dlg()
+
+        dlg_proc(self.h_dlg, DLG_SHOW_MODAL)
+        dlg_proc(self.h_dlg, DLG_FREE)
 
 
-    def init_editor_dlg(self, filename, text):
+    def init_editor_dlg(self):
 
         h=dlg_proc(0, DLG_CREATE)
         dlg_proc(h, DLG_PROP_SET, prop={
-            'cap': 'Unsaved changes: '+filename,
+            'cap': 'Unsaved changes: '+self.filename,
             'w': 900,
             'h': 500,
             'resize': True,
@@ -39,14 +41,19 @@ class Command:
         n=dlg_proc(h, DLG_CTL_ADD, 'editor')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={
             'name': 'ed',
-            'align': ALIGN_CLIENT,
-            'sp_a': 6,
-            #'h': 450,
+            'x': 6,
+            'y': 6,
+            'a_r': ('', ']'),
+            'a_b': ('', ']'),
+            'sp_l': 6,
+            'sp_t': 6,
+            'sp_r': 6,
+            'sp_b': 38,
             })
 
         h_editor = dlg_proc(h, DLG_CTL_HANDLE, index=n)
         ed0 = Editor(h_editor)
-        ed0.set_text_all(text)
+        ed0.set_text_all(self.text)
         ed0.set_prop(PROP_MICROMAP, False)
         ed0.set_prop(PROP_MINIMAP, False)
         ed0.set_prop(PROP_RULER, False)
@@ -55,6 +62,46 @@ class Command:
         ed0.set_prop(PROP_RO, True)
         ed0.set_prop(PROP_LEXER_FILE, 'Diff')
 
-        dlg_proc(h, DLG_CTL_FOCUS, index=n)
+        n=dlg_proc(h, DLG_CTL_ADD, 'button')
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={
+            'name': 'btn_close',
+            'cap': 'Close',
+            'w': 110,
+            'a_l': None,
+            'a_t': None,
+            'a_b': ('', ']'),
+            'a_r': ('', ']'),
+            'sp_a': 6,
+            'on_change': self.callback_btn_close,
+            })
 
+        n=dlg_proc(h, DLG_CTL_ADD, 'button')
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={
+            'name': 'btn_save',
+            'cap': 'Save as...',
+            'w': 110,
+            'a_l': None,
+            'a_t': None,
+            'a_b': ('', ']'),
+            'a_r': ('btn_close', '['),
+            'sp_a': 6,
+            'on_change': self.callback_btn_save,
+            })
+
+        dlg_proc(h, DLG_CTL_FOCUS, name='ed')
         return h
+
+
+    def callback_btn_close(self, id_dlg, id_ctl, data='', info=''):
+
+        dlg_proc(self.h_dlg, DLG_HIDE)
+
+
+    def callback_btn_save(self, id_dlg, id_ctl, data='', info=''):
+
+        res = dlg_file(False, self.filename+'.diff', '', '')
+        if not res: return
+
+        with open(res, 'w') as f:
+            f.write(self.text)
+        msg_status('Saved: '+res)
