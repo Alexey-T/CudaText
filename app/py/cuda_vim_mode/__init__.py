9faa9fb0a7773b5839b4569c588a0c17113a83f2
Alexey-t <support@uvviewsoft.com>
2017-09-29 18:52:24 +0300

    work on Vim

--- app/py/cuda_vim_mode/__init__.py
+++ app/py/cuda_vim_mode/__init__.py
@@ -33,59 +33,60 @@ class Command:
             return
 
         if state in ['', 's']:
-            if key==ord('H'):
+            if key==ord('H') and state=='':
                 ed.cmd(cc.cCommand_KeyLeft)
                 msg('left')
                 return False
 
-            if key==ord('J'):
+            if key==ord('J') and state=='':
                 ed.cmd(cc.cCommand_KeyDown)
                 msg('down')
                 return False
 
-            if key==ord('K'):
+            if key==ord('K') and state=='':
                 ed.cmd(cc.cCommand_KeyUp)
                 msg('up')
                 return False
 
-            if key==ord('L'):
+            if key==ord('L') and state=='':
                 ed.cmd(cc.cCommand_KeyRight)
                 msg('right')
                 return False
 
-            if key==ord('B'):
+            if key==ord('B') and state=='':
                 ed.cmd(cc.cCommand_GotoWordPrev)
                 msg('go to prev word')
                 return False
 
-            if key==ord('W'):
+            if key==ord('W') and state=='':
                 ed.cmd(cc.cCommand_GotoWordNext)
                 msg('go to next word')
                 return False
 
-            if key==ord('E'):
+            if key==ord('E') and state=='':
                 goto_word_end()
                 msg('go to word end')
                 return False
 
-            if key==ord('A'):
+            if key==ord('A') and state=='':
                 ed.cmd(cc.cCommand_KeyRight)
                 self.ins = True
                 msg('insertion mode, after current char')
                 return False
 
-            if key==ord('I'):
+            if key==ord('I') and state=='':
                 self.ins = True
                 msg('insertion mode, at current char')
                 return False
 
-            if key==ord('X'):
-                if state=='s':
-                    ed.cmd(cc.cCommand_KeyBackspace)
-                    msg('delete char left')
-                else:
-                    ed.cmd(cc.cCommand_KeyDelete)
-                    msg('delete char')
+            if key==ord('X') and state=='':
+                ed.cmd(cc.cCommand_KeyDelete)
+                msg('delete char')
+                return False
+
+            if key==ord('X') and state=='s':
+                ed.cmd(cc.cCommand_KeyBackspace)
+                msg('delete char left')
                 return False
 
             #block letters
