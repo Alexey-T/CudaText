6cca8f2b318e06871b09c22ad97075ff83dea8fe
Alexey-t <support@uvviewsoft.com>
2017-09-30 10:36:14 +0300

    Api event on_insert

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -15,8 +15,8 @@ uses
   Classes, SysUtils;
 
 const
-  cAppExeVersion = '1.20.0.0';
-  cAppApiVersion = '1.0.199';
+  cAppExeVersion = '1.20.2.0';
+  cAppApiVersion = '1.0.200';
 
 const
   msgTitle = 'CudaText'; //no need i18n
