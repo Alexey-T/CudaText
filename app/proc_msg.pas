21efd852f5fd98ed0c6d921f83d253287d42a1dc
Alexey-t <support@uvviewsoft.com>
2017-10-04 11:52:22 +0300

    fixing #1105

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -15,7 +15,7 @@ uses
   Classes, SysUtils;
 
 const
-  cAppExeVersion = '1.21.0.0';
+  cAppExeVersion = '1.21.0.1';
   cAppApiVersion = '1.0.201';
 
 const
--- setup/cuda_ver.sh
+++ setup/cuda_ver.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cuda_ver=1.21.0.0
+cuda_ver=1.21.0.1
