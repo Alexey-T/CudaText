a66fc043766238c7e754062dc6eeb5d33dda4267
Alexey-t <support@uvviewsoft.com>
2017-11-23 19:46:01 +0300

    delete empty method

--- app/formmain.pas
+++ app/formmain.pas
@@ -415,7 +415,6 @@ type
     procedure FrameAddRecent(Sender: TObject);
     procedure FrameOnMsgStatus(Sender: TObject; const AStr: string);
     procedure FrameOnChangeCaretPos(Sender: TObject);
-    procedure FrameParseBegin(Sender: TObject);
     procedure FrameParseDone(Sender: TObject);
     procedure ListboxOutClick(Sender: TObject);
     procedure ListboxOutDrawItem(Sender: TObject; C: TCanvas; AIndex: integer;
