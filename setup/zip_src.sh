c51d42abdd68588ba40c12ac765e91d5a5bbe832
Alexey-t <support@uvviewsoft.com>
2017-11-24 10:47:11 +0300

    fix script

--- setup/zip_src.sh
+++ setup/zip_src.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+. ./cuda_ver.sh
+
 mkdir ~/cudatext_build/src
 cd ~/cudatext_build/src
 curl -L https://github.com/Alexey-T/CudaText/archive/master.zip >CudaText.zip
@@ -12,4 +14,4 @@ curl -L https://github.com/Alexey-T/EControl/archive/master.zip >EControl.zip
 curl -L https://github.com/Alexey-T/Python-for-Lazarus/archive/master.zip >Python.zip
 
 cd ..
-zip -r cudatext-src.zip src/
+zip -r cudatext-src-$cuda_ver.zip src/
