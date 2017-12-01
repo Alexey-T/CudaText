c47f3db29805a9864cd06f2d78bae3cd75d9318c
Alexey-t <support@uvviewsoft.com>
2017-12-01 09:53:37 +0300

    AddonMan: show nice progress in statusbar on installing

--- app/py/cuda_addonman/__init__.py
+++ app/py/cuda_addonman/__init__.py
@@ -32,6 +32,10 @@ STD_MODULES = (
 
 
 class Command:
+    tick = 0
+    tick_inc = +1
+    tick_msg = ''
+
     def __init__(self):
         if os.path.isfile(fn_config):
             data = json.loads(open(fn_config).read(), object_pairs_hook=collections.OrderedDict)
@@ -149,14 +153,20 @@ class Command:
                 return
 
         #download
-        msg_status('Downloading file...')
+        self.tick_msg = 'Downloading file'
+        timer_proc(TIMER_START, self.timer_tick, 500)
+
         fn = get_plugin_zip(url)
         if not os.path.isfile(fn):
+            timer_proc(TIMER_STOP, self.timer_tick, 0)
             msg_status('Cannot download file')
             return
-        msg_status('Opened downloaded file')
+
+        self.tick_msg = 'Opening downloaded zip'
         s_options = '' if opt.install_confirm else '/silent'
         file_open(fn, options=s_options)
+
+        timer_proc(TIMER_STOP, self.timer_tick, 0)
         msg_status('Addon installed')
 
         #save version
@@ -381,3 +391,13 @@ class Command:
     def install_from_github(self):
 
         do_install_from_github()
+
+    def timer_tick(self, tag='', info=''):
+
+        SIZE = 20
+        self.tick += self.tick_inc
+        if (self.tick<=0) or (self.tick>=SIZE):
+            self.tick_inc = -self.tick_inc
+
+        text = '['+'_'*self.tick+'*'+'_'*(SIZE-self.tick)+'] '+self.tick_msg
+        msg_status(text, True)
