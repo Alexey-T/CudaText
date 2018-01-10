c3c49cbf0202c3a4265890ab6b74474b7fae7c77
Alexey-t <support@uvviewsoft.com>
2018-01-10 23:49:16 +0300

    fix in cudatext_init.py

--- app/py/cudatext_init.py
+++ app/py/cudatext_init.py
@@ -1,5 +1,6 @@
-import sys
+# this is context of app, "sys" already imported
 _v = sys.version_info
 print("Python %d.%d.%d" % (_v[0], _v[1], _v[2]) )
 
+# can comment this line to speedup, it's to test API in console
 from cudatext import *
