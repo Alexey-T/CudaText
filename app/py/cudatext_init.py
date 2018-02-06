4a5ee0a5c55bc87d28950ad57620c5af4b9f10aa
OlehL <lom@index.ua>
2018-02-06 09:01:27 +0200

    support utf-8
    I tested it only on windows os.

--- app/py/cudatext_init.py
+++ app/py/cudatext_init.py
@@ -1,3 +1,6 @@
+import os;      os.putenv("PYTHONIOENCODING",'UTF-8')
+import sys;     sys.getfilesystemencoding = lambda: 'UTF-8'
+import locale;  locale.getpreferredencoding = lambda: 'utf-8'
 # this is context of app, "sys" already imported
 _v = sys.version_info
 print("Python %d.%d.%d" % (_v[0], _v[1], _v[2]) )
