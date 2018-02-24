803eac3125fbd0cb80127fb60b5c004430e84148
Alexey-t <support@uvviewsoft.com>
2018-02-24 19:20:45 +0300

    fix in MakePlugin

--- app/py/cuda_make_plugin/dlg.py
+++ app/py/cuda_make_plugin/dlg.py
@@ -4,7 +4,7 @@ from cudatext import *
 from .events import *
 
 def is_correct_id(name):
-    if not name: 
+    if not name:
         return False
     if not name[0] in string.ascii_letters+'_':
         return False
@@ -20,8 +20,8 @@ def get_module_dir():
         name = 'sample'+str(i+1)
         if not os.path.isdir(os.path.join(dir, 'cuda_'+name)):
             return name
-    return 'sample' 
-         
+    return 'sample'
+
 
 def dlg_make_plugin():
     '''
@@ -31,24 +31,24 @@ def dlg_make_plugin():
     dlg_w = 406
     dlg_h = 460
     btn_w = 80
-    
+
     id_name = 1
     id_module = 3
     id_items = 5
     id_events = 7
     id_ok = 8
     c1 = chr(1)
-    
+
     s_caption = 'MySample'
     s_module = get_module_dir()
     s_plugin_items = 'MySample>run'
     s_events_checks = ''
-    
+
     while True:
         res = dlg_custom('Make Plugin', dlg_w, dlg_h, '\n'.join([]
           + [c1.join(['type=label', 'cap=Plugin &name', 'pos=6,6,400,0'])]
           + [c1.join(['type=edit', 'val='+s_caption, 'pos=6,24,400,46'])]
-          + [c1.join(['type=label', 'cap=Lowercase &module name', 'pos=6,54,400,0'])]
+          + [c1.join(['type=label', 'cap=&Module name after "cuda_"', 'pos=6,54,400,0'])]
           + [c1.join(['type=edit', 'val='+s_module, 'pos=6,72,400,46'])]
           + [c1.join(['type=label', 'cap=&Items "Caption>method" ("-" at end: hide item in Plugins)', 'pos=6,102,400,0'])]
           + [c1.join(['type=memo', 'val='+s_plugin_items, 'pos=6,120,400,240'])]
@@ -61,17 +61,17 @@ def dlg_make_plugin():
         (btn, text) = res
         if btn!=id_ok: return
         text = text.splitlines()
-    
+
         s_caption = text[id_name]
         s_module = text[id_module]
         s_plugin_items = text[id_items]
         s_events_checks = text[id_events]
-        
+
         items = text[id_items].split('\t')
         cmd_list = [s.split('>') for s in items if s]
         items = text[id_events].split(';')[1].split(',')
         event_list = [s for (n, s) in enumerate(EVENTS) if items[n]=='1']
-        
+
         if not s_module or not s_caption:
             msg_box('Empty field', MB_OK+MB_ICONERROR)
             continue
@@ -85,12 +85,12 @@ def dlg_make_plugin():
                 msg_box('Incorrect item: '+repr(i), MB_OK+MB_ICONERROR)
                 bad = True
                 break
-                
+
             if not i[0]:
                 msg_box('Incorrect item name: '+i[0], MB_OK+MB_ICONERROR)
                 bad = True
                 break
-                
+
             s = i[1]
             if s.endswith('-'): s=s[:-1]
             if not is_correct_id(s):
@@ -100,7 +100,7 @@ def dlg_make_plugin():
 
         if bad:
             continue
-                
+
         #handle "-" at end of cmd:
         for i in cmd_list:
             nomenu = i[1].endswith('-')
