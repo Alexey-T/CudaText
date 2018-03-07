27c58e7ed425931607d55ba070098f965738f727
Alexey-t <support@uvviewsoft.com>
2018-03-07 20:21:08 +0300

    ver

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -15,7 +15,7 @@ uses
   Classes, SysUtils;
 
 const
-  cAppExeVersion = '1.44.2.0';
+  cAppExeVersion = '1.45.0.0';
   cAppApiVersion = '1.0.230';
 
 const
--- setup/cuda_ver.sh
+++ setup/cuda_ver.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cuda_ver=1.44.2.0
+cuda_ver=1.45.0.0
