56f7ae68b25855365cc9099394f542330864667a
Alexey-t <support@uvviewsoft.com>
2018-03-29 14:08:39 +0300

    improved auto-closing bracket: now when you type ")" after "(", closing bracket not duplicated

--- app/formframe.pas
+++ app/formframe.pas
@@ -904,9 +904,48 @@ begin
   DoPyEvent(Sender as TATSynEdit, cEventOnFocus, []);
 end;
 
+function _GetPairForCloseBracket(ch: char): char;
+begin
+  case ch of
+    ')': Result:= '(';
+    ']': Result:= '[';
+    '}': Result:= '{';
+    else Result:= #0;
+  end;
+end;
+
 procedure TEditorFrame.EditorOnCommand(Sender: TObject; ACmd: integer;
   const AText: string; var AHandled: boolean);
+var
+  Ed: TATSynEdit;
+  Caret: TATCaretItem;
+  Str: atString;
+  ch: char;
 begin
+  Ed:= Sender as TATSynEdit;
+  if Ed.Carets.Count>0 then
+  begin
+    Caret:= Ed.Carets[0];
+
+    //improve auto-closing brackets, avoid duplicate ')' after '('
+    if Ed.Strings.IsIndexValid(Caret.PosY) then
+      if Length(AText)=1 then
+      begin
+        ch:= _GetPairForCloseBracket(AText[1]);
+        if (ch<>#0) and (Pos(ch, UiOps.AutoCloseBrackets)>0) then
+        begin
+          Str:= Ed.Strings.Lines[Caret.PosY];
+          if (Caret.PosX>0) and (Caret.PosX<Length(Str)) then
+            if Copy(Str, Caret.PosX, 2) = ch+AText then
+            begin
+              Ed.DoCommand(cCommand_KeyRight);
+              AHandled:= true;
+              exit;
+            end;
+        end;
+      end;
+  end;
+
   if Assigned(FOnEditorCommand) then
     FOnEditorCommand(Sender, ACmd, AText, AHandled);
 end;
