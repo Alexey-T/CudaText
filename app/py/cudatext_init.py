ae56b082316ab8232af5aeb0eb94bdb9fde694ad
Alexey-t <support@uvviewsoft.com>
2018-11-12 20:33:53 +0300

    more checks for old crap files/dirs

--- app/py/cudatext_init.py
+++ app/py/cudatext_init.py
@@ -8,8 +8,13 @@ print("Python %d.%d.%d" % (_v[0], _v[1], _v[2]) )
 # it's to test API in console
 from cudatext import *
 
-fn = os.path.join(app_path(APP_DIR_PY), 'cudax_lib')
-if os.path.isdir(fn):
-    msg_box('CudaText has found that old folder'+
-            ' "%s" still exists, but it must be removed. Please delete it manually.'%fn, 
-            MB_OK+MB_ICONWARNING)
+# check old crap files/dirs
+bads = [
+    os.path.join(app_path(APP_DIR_PY), 'cudax_lib'),
+    os.path.join(app_path(APP_DIR_PY), 'requests'),
+    os.path.join(app_path(APP_DIR_SETTINGS), 'lexer styles backup.ini'),
+    ]
+for fn in bads:
+    if os.path.exists(fn):
+        msg_box('CudaText has found that old file/folder still exists:\n%s\n\nPlease delete it manually.'%fn, 
+                MB_OK+MB_ICONWARNING)
