86d67b76393d5a305e0f5fa7d0f00345ddf9403f
Alexey-t <support@uvviewsoft.com>
2019-01-30 11:59:23 +0300

    optimize cudatext_init.py

--- app/py/cudatext_init.py
+++ app/py/cudatext_init.py
@@ -9,9 +9,7 @@ print("Python %d.%d.%d" % sys.version_info[:3])
 bads = [
     os.path.join(app_path(APP_DIR_PY), 'cudax_lib'),
     os.path.join(app_path(APP_DIR_PY), 'requests'),
-    os.path.join(app_path(APP_DIR_SETTINGS), 'lexer styles backup.ini'),
     ]
 for fn in bads:
     if os.path.exists(fn):
-        msg_box('CudaText has found that old file/folder still exists:\n%s\n\nPlease delete it manually.'%fn, 
-                MB_OK+MB_ICONWARNING)
+        msg_box('CudaText has found that old file/folder still exists:\n%s\n\nPlease delete it manually.'%fn, MB_OK+MB_ICONWARNING)
