7429b2ed4943667d4684745d9d0af21e2f9c6e0b
Alexey-t <support@uvviewsoft.com>
2019-02-10 14:52:43 +0300

    begin work on treehelpers

--- app/formmain.pas
+++ app/formmain.pas
@@ -1083,6 +1083,7 @@ type
     property SidebarPanel: string read FLastSidebarPanel write SetSidebarPanel;
     function DoPyEvent(AEd: TATSynEdit; AEvent: TAppPyEvent; const AParams: array of string): string;
     procedure DoPyCommand(const AModule, AMethod: string; const AParams: array of string);
+    function DoPyTreeHelper(Frame: TEditorFrame): boolean;
     //function DoPyCallbackFromAPI(const ACallback: string; const AParams: array of string): string;
   end;
 
