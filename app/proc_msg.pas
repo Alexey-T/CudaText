fdcef1291d87b8edbc55f888d40a643cae0cf75a
Alexey-t <support@uvviewsoft.com>
2019-02-15 00:27:13 +0300

    del file_save(), fix #1850

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -16,8 +16,8 @@ uses
   ATSynEdit;
 
 const
-  cAppExeVersion = '1.74.6.0';
-  cAppApiVersion = '1.0.273';
+  cAppExeVersion = '1.74.7.0';
+  cAppApiVersion = '1.0.274';
 
 const
   cOptionSystemSuffix =
