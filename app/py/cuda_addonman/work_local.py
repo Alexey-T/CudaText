3600088b5a33779dc92500f41d9d1f4ed44e7caf
Alexey-t <support@uvviewsoft.com>
2019-02-25 14:43:30 +0300

    AddonMan fix

--- app/py/cuda_addonman/work_local.py
+++ app/py/cuda_addonman/work_local.py
@@ -208,7 +208,7 @@ def get_installed_items_ex(exclude_modules):
         'kind': 'lexer',
         'name': i,
         'files': [
-            os.path.join(d, i)+'.lcf',
+            os.path.join(d, i+'.lcf'),
             os.path.join(d, i+'.cuda-lexmap'),
             os.path.join(d_acp, i+'.acp'),
             ],
@@ -221,7 +221,7 @@ def get_installed_items_ex(exclude_modules):
         'kind': 'lexer',
         'name': i+' ^',
         'files': [
-            os.path.join(d, i)+'.cuda-litelexer',
+            os.path.join(d, i+'.cuda-litelexer'),
             ],
         } for i in l]
 
