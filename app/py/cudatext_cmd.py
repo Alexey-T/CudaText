b955cdf6f0c529519765e5df15bcd57d93267bda
Alexey-t <support@uvviewsoft.com>
2019-04-07 15:32:07 +0300

    rename 2 new cmds

--- app/py/cudatext_cmd.py
+++ app/py/cudatext_cmd.py
@@ -45,8 +45,8 @@ cCommand_ColSelectWithoutKey_On = 130
 cCommand_ColSelectWithoutKey_Off = 131
 cCommand_ColSelectWithoutKey_Toggle = 132
 
-cCommand_DeselectFirstSel = 135
-cCommand_DeselectLastSel = 136
+cCommand_RemoveFirstCaret = 135
+cCommand_RemoveLastCaret = 136
 
 cCommand_TextInsert = 150
 cCommand_TextInsertTabChar = 151
