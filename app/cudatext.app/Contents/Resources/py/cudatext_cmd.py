baeceb47d0b649aa4398d6a419655936c72ca99e
Alexey-t <support@uvviewsoft.com>
2019-04-08 21:19:25 +0300

    mac up

--- app/cudatext.app/Contents/Resources/py/cudatext_cmd.py
+++ app/cudatext.app/Contents/Resources/py/cudatext_cmd.py
@@ -45,6 +45,9 @@ cCommand_ColSelectWithoutKey_On = 130
 cCommand_ColSelectWithoutKey_Off = 131
 cCommand_ColSelectWithoutKey_Toggle = 132
 
+cCommand_RemoveFirstCaret = 135
+cCommand_RemoveLastCaret = 136
+
 cCommand_TextInsert = 150
 cCommand_TextInsertTabChar = 151
 cCommand_KeyBackspace = 152
