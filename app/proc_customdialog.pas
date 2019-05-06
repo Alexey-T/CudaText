26fbc4195efa80035841a102a709f79ffb98adc4
Alexey-t <support@uvviewsoft.com>
2019-05-06 15:31:52 +0300

    fix scale

--- app/proc_customdialog.pas
+++ app/proc_customdialog.pas
@@ -773,7 +773,7 @@ begin
   begin
     Ctl:= TATListbox.Create(AForm);
     TATListbox(Ctl).VirtualMode:= false;
-    TATListbox(Ctl).ItemHeight:= GetListboxItemHeight(UiOps.VarFontName, UiOps.VarFontSize);;
+    TATListbox(Ctl).ItemHeight:= AppScale(GetListboxItemHeight(UiOps.VarFontName, UiOps.VarFontSize));;
     TATListbox(Ctl).CanGetFocus:= true;
     TATListbox(Ctl).OnChangedSel:= @AForm.DoOnChange;
     TATListbox(Ctl).OnDrawItem:= @AForm.DoOnListboxDrawItem;
