e634ae1a6a08c6633c5c58d217ecbd324cc90d06
Alexey-t <support@uvviewsoft.com>
2019-05-18 12:46:23 +0300

    fix zip_src.sh

--- setup/zip_src.sh
+++ setup/zip_src.sh
@@ -7,10 +7,10 @@ curl -L https://github.com/Alexey-T/ATBinHex-Lazarus/archive/master.zip >ATBinHe
 curl -L https://github.com/Alexey-T/ATFileNotif-Lazarus/archive/master.zip >ATFileNotif.zip
 curl -L https://github.com/Alexey-T/ATFlatControls/archive/master.zip >ATFlatControls.zip
 curl -L https://github.com/Alexey-T/ATSynEdit/archive/master.zip >ATSynEdit.zip
-curl -L https://github.com/Alexey-T/ATSynEdit_Ex/archive/master.zip >ATSynEdit_Ex.zip
+curl -L https://github.com/Alexey-T/ATSynEdit_Ex_v1/archive/master.zip >ATSynEdit_Ex_v1.zip
 curl -L https://github.com/Alexey-T/ATTabs/archive/master.zip >ATTabs.zip
 curl -L https://github.com/Alexey-T/CudaText/archive/master.zip >CudaText.zip
-curl -L https://github.com/Alexey-T/EControl/archive/master.zip >EControl.zip
+curl -L https://github.com/Alexey-T/EControl_v1/archive/master.zip >EControl_v1.zip
 curl -L https://github.com/Alexey-T/Python-for-Lazarus/archive/master.zip >Python.zip
 
 cd ..
