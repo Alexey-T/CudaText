f7b3a7f11b0708b1bdf8a26b5e2dd449859089fe
Alexey-t <support@uvviewsoft.com>
2019-05-27 11:34:35 +0300

    fix zip_src.sh

--- setup/zip_src.sh
+++ setup/zip_src.sh
@@ -7,6 +7,7 @@ curl -L https://github.com/Alexey-T/ATBinHex-Lazarus/archive/master.zip >ATBinHe
 curl -L https://github.com/Alexey-T/ATFileNotif-Lazarus/archive/master.zip >ATFileNotif.zip
 curl -L https://github.com/Alexey-T/ATFlatControls/archive/master.zip >ATFlatControls.zip
 curl -L https://github.com/Alexey-T/ATSynEdit/archive/master.zip >ATSynEdit.zip
+curl -L https://github.com/Alexey-T/ATSynEdit_Cmp/archive/master.zip >ATSynEdit_Cmp.zip
 curl -L https://github.com/Alexey-T/ATSynEdit_Ex_v1/archive/master.zip >ATSynEdit_Ex_v1.zip
 curl -L https://github.com/Alexey-T/ATTabs/archive/master.zip >ATTabs.zip
 curl -L https://github.com/Alexey-T/CudaText/archive/master.zip >CudaText.zip
