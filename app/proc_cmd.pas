ea559e4cc00d62e774ae2c886f90c4b4414cb4a2
Alexey-t <support@uvviewsoft.com>
2019-06-05 15:59:36 +0300

    rename new command id

--- app/proc_cmd.pas
+++ app/proc_cmd.pas
@@ -134,7 +134,7 @@ const
   cmd_SwitchTab_SimpleNext = 2612;
   cmd_SwitchTab_SimplePrev = 2613;
   cmd_SwitchTab_Dialog     = 2614;
-  cmd_SwitchTab_RecentPrev = 2615;
+  cmd_SwitchTab_Recent     = 2615;
 
   cmd_SplitTabToggle   = 2620;
   cmd_SplitTabHorzVert = 2621;
@@ -379,7 +379,7 @@ begin
   M.Add(cmd_SwitchTab_SimpleNext, 'ui: switch tab, simply to next', [], []);
   M.Add(cmd_SwitchTab_SimplePrev, 'ui: switch tab, simply to previous', [], []);
   M.Add(cmd_SwitchTab_Dialog, 'ui: switch tab, dialog', [], []);
-  M.Add(cmd_SwitchTab_RecentPrev, 'ui: switch tab, to recent previous', [], []);
+  M.Add(cmd_SwitchTab_Recent, 'ui: switch tab, to recent previous', [], []);
 
   M.Add(cmd_ShowMainMenuAsPopup, 'ui: show main menu as popup', [], []);
   M.Add(cmd_DialogSaveTabs, 'dialog: save tabs', [], []);
