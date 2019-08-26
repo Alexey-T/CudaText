213c8eddf2ab26952a6cce80fa7946378f6050af
Alexey-t <support@uvviewsoft.com>
2019-08-26 20:40:28 +0300

    fix console err msg

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -123,7 +123,7 @@ const
   msgEndUnix: string = 'LF';
   msgEndMac: string = 'CR';
 
-  msgCannotInitPython1: string = 'No Python engine (3.x) found. Python plugins don''t work now. To make it ok:';
+  msgCannotInitPython1: string = 'No Python 3 engine found. Python plugins don''t work now. To fix this:';
   {$ifdef darwin}
   msgCannotInitPython2: string = 'install Python 3.x from www.python.org, it should be found by CudaText then.';
   {$else}
