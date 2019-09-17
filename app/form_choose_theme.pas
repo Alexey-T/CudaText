acef66b5dac55c3eb8267c9ec3e2c99b361dc0dd
Alexey-t <support@uvviewsoft.com>
2019-09-17 22:49:04 +0300

    i18n for choose theme dialog

--- app/form_choose_theme.pas
+++ app/form_choose_theme.pas
@@ -6,9 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ButtonPanel,
-  FileUtil,
-  LazFileUtils,
-  Math,
+  FileUtil, LazFileUtils, IniFiles, Math,
   proc_msg,
   proc_globdata;
 
@@ -44,6 +42,29 @@ implementation
 
 {$R *.lfm}
 
+procedure DoLocalize_FormChooseTheme(F: TfmChooseTheme);
+const
+  section = 'd_theme';
+var
+  ini: TIniFile;
+  fn: string;
+begin
+  fn:= GetAppLangFilename;
+  if not FileExists(fn) then exit;
+  ini:= TIniFile.Create(fn);
+  try
+    with F do Caption:= ini.ReadString(section, '_', Caption);
+    with F.ButtonPanel1.OKButton do Caption:= msgButtonOk;
+    with F.ButtonPanel1.CancelButton do Caption:= msgButtonCancel;
+
+    with F.chkSync do Caption:= ini.ReadString(section, 'syn', Caption);
+    with F.GroupUI do Caption:= ini.ReadString(section, 'ty_ui', Caption);
+    with F.GroupSyntax do Caption:= ini.ReadString(section, 'ty_sy', Caption);
+  finally
+    FreeAndNil(ini);
+  end;
+end;
+
 { TfmChooseTheme }
 
 procedure TfmChooseTheme.FormCreate(Sender: TObject);
@@ -52,6 +73,8 @@ var
   Files_sy: TStringList;
   s: string;
 begin
+  DoLocalize_FormChooseTheme(Self);
+
   ListboxUI.Items.Clear;
   ListboxSyntax.Items.Clear;
 
