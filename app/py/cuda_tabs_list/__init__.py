31ae45083e2fe4131a555fb8007223c270a14e6c
Alexey-t <support@uvviewsoft.com>
2019-09-27 18:00:16 +0300

    TabsList: write comment to config

--- app/py/cuda_tabs_list/__init__.py
+++ app/py/cuda_tabs_list/__init__.py
@@ -48,6 +48,9 @@ class Command:
         ini_write(fn_config, 'op', 'show_index_aligned', bool_to_str(self.show_index_aligned))
         ini_write(fn_config, 'op', 'font_name', self.font_name)
         ini_write(fn_config, 'op', 'font_size', str(self.font_size))
+        
+        ini_write(fn_config, 'columns', '; width_ values: >0 - in pixels, <0 - in percents, =0 - auto-stretched', '')
+        ini_write(fn_config, 'columns', '; show_ values: boolean, 0 or 1', '')
         ini_write(fn_config, 'columns', 'width_name', str(self.column_name))
         ini_write(fn_config, 'columns', 'width_folder', str(self.column_folder))
         ini_write(fn_config, 'columns', 'width_lexer', str(self.column_lexer))
