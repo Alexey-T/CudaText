5638c3c282a86d1e5cc2dc6dbbe1f44892632a82
Alexey-t <support@uvviewsoft.com>
2019-10-28 18:25:55 +0300

    ed.attr change def param val

--- app/py/cudatext.py
+++ app/py/cudatext.py
@@ -1188,17 +1188,15 @@ class Editor:
     def markers(self, id, x=0, y=0, tag=0, len_x=0, len_y=0):
         return ct.ed_markers(self.h, id, x, y, tag, len_x, len_y)
 
-    def attr(self, id, tag=0, x=0, y=0, len=0,
+    def attr(self, id, tag=0, x=0, y=0, len=1,
              color_font=COLOR_NONE, color_bg=COLOR_NONE, color_border=COLOR_NONE,
              font_bold=0, font_italic=0, font_strikeout=0,
              border_left=0, border_right=0, border_down=0, border_up=0,
              show_on_map=False, map_only=False
              ):
         column = 1 if show_on_map==True else -1 if show_on_map==False else show_on_map
-        return ct.ed_attr(self.h, id, tag, 
-                          to_str(x), 
-                          to_str(y), 
-                          to_str(len),
+        return ct.ed_attr(self.h, id, tag,
+                          to_str(x), to_str(y), to_str(len),
                           color_font, color_bg, color_border,
                           font_bold, font_italic, font_strikeout,
                           border_left, border_right, border_down, border_up,
