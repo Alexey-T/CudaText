133ebcc598545897574ffdd03543c4598a3e6d7a
Alexey-t <support@uvviewsoft.com>
2019-10-29 09:45:38 +0300

    vers

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -16,7 +16,7 @@ uses
   ATSynEdit;
 
 const
-  cAppExeVersion = '1.88.8.0';
+  cAppExeVersion = '1.89.0.0';
   cAppApiVersion = '1.0.310';
 
 const
--- setup/cuda_ver.sh
+++ setup/cuda_ver.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cuda_ver=1.88.8.0
+cuda_ver=1.89.0.0
