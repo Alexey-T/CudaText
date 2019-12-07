61a693efc8aebf8c2badca9daa1c33f3be9b6c01
Alexey-t <support@uvviewsoft.com>
2019-12-07 08:46:15 +0300

    fix setup scripts

--- setup/z_win_x32.sh
+++ setup/z_win_x32.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir ~/cudatext_build
-./win_common.sh x32 ~/cudatext_build ../app/builds/win32 ~/cudatext_build/py_dll_x32
+./win_common.sh x32 ~/cudatext_build ../app/builds/win32 ~/cudatext_build/lib_win_x32
--- setup/z_win_x64.sh
+++ setup/z_win_x64.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir ~/cudatext_build
-./win_common.sh x64 ~/cudatext_build ../app/builds/win64 ~/cudatext_build/py_dll_x64
+./win_common.sh x64 ~/cudatext_build ../app/builds/win64 ~/cudatext_build/lib_win_x64
