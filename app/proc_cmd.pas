914118b6a0a9aa3b3324854eb692a315d48530fd
Alexey-t <support@uvviewsoft.com>
2019-12-15 21:15:46 +0300

    add cmd palette commands to reload in iso-15

--- app/proc_cmd.pas
+++ app/proc_cmd.pas
@@ -244,6 +244,7 @@ const
   cmd_Encoding_mac_NoReload       = 2724;
   cmd_Encoding_iso1_NoReload      = 2725;
   cmd_Encoding_iso2_NoReload      = 2726;
+  cmd_Encoding_iso15_NoReload     = 2727;
   cmd_Encoding_cp437_NoReload     = 2730;
   cmd_Encoding_cp850_NoReload     = 2731;
   cmd_Encoding_cp852_NoReload     = 2732;
@@ -272,6 +273,7 @@ const
   cmd_Encoding_mac_Reload       = 2764;
   cmd_Encoding_iso1_Reload      = 2765;
   cmd_Encoding_iso2_Reload      = 2766;
+  cmd_Encoding_iso15_Reload     = 2767;
   cmd_Encoding_cp437_Reload     = 2770;
   cmd_Encoding_cp850_Reload     = 2771;
   cmd_Encoding_cp852_Reload     = 2772;
@@ -543,6 +545,7 @@ begin
   M.Add(cmd_Encoding_mac_NoReload, 'change encoding, no reload: mac', [], []);
   M.Add(cmd_Encoding_iso1_NoReload, 'change encoding, no reload: iso1', [], []);
   M.Add(cmd_Encoding_iso2_NoReload, 'change encoding, no reload: iso2', [], []);
+  M.Add(cmd_Encoding_iso15_NoReload, 'change encoding, no reload: iso15', [], []);
   M.Add(cmd_Encoding_cp437_NoReload, 'change encoding, no reload: cp437', [], []);
   M.Add(cmd_Encoding_cp850_NoReload, 'change encoding, no reload: cp850', [], []);
   M.Add(cmd_Encoding_cp852_NoReload, 'change encoding, no reload: cp852', [], []);
@@ -571,6 +574,7 @@ begin
   M.Add(cmd_Encoding_mac_Reload, 'change encoding, reload: mac', [], []);
   M.Add(cmd_Encoding_iso1_Reload, 'change encoding, reload: iso1', [], []);
   M.Add(cmd_Encoding_iso2_Reload, 'change encoding, reload: iso2', [], []);
+  M.Add(cmd_Encoding_iso15_Reload, 'change encoding, reload: iso15', [], []);
   M.Add(cmd_Encoding_cp437_Reload, 'change encoding, reload: cp437', [], []);
   M.Add(cmd_Encoding_cp850_Reload, 'change encoding, reload: cp850', [], []);
   M.Add(cmd_Encoding_cp852_Reload, 'change encoding, reload: cp852', [], []);
