af1ffa0e58f23a4689c5874aedf135da75f47bb5
Alexey-t <support@uvviewsoft.com>
2020-01-18 15:16:43 +0300

    Artem's work on #2366

--- app/proc_py.pas
+++ app/proc_py.pas
@@ -22,8 +22,8 @@ procedure Py_SetSysPath(const Dirs: array of string; DoAdd: boolean);
 function Py_RunPlugin_Command(const AModule, AMethod: string; const AParams: array of string): string;
 function Py_RunPlugin_Event(const AModule, ACmd: string;
   AEd: TATSynEdit; const AParams: array of string; ALazy: boolean): string;
-function Py_RunModuleFunction(const AModule, AFunc: string; AParams: array of string): PPyObject;
-function Py_RunModuleFunction2(const AModule, AFunc: string; AParams: array of PPyObject): PPyObject;
+function Py_RunModuleFunction(const AModule, AFunc: string; AParams: array of PPyObject;const AParamNames: array of string): PPyObject;
+function Py_RunModuleFunction(const AModule, AFunc: string; AParams: array of PPyObject): PPyObject;
 
 function Py_rect(const R: TRect): PPyObject; cdecl;
 function Py_rect_monitor(N: Integer): PPyObject; cdecl;
@@ -180,6 +180,7 @@ begin
 end;
 
 
+(*
 function Py_RunModuleFunction(const AModule, AFunc: string; AParams: array of string): PPyObject;
 var
   SCmd1, SCmd2: string;
@@ -196,32 +197,54 @@ begin
   except
   end;
 end;
-
-(*
-// bug: it dont return result, always gets Py_None
-function Py_RunModuleFunction(const AModule, AFunc: string; AParams: array of string): PPyObject;
-var
-  ObjParams: array of PPyObject;
-  i: integer;
-begin
-  with GetPythonEngine do
-  begin
-    SetLength(ObjParams, Length(AParams));
-    for i:= 0 to Length(AParams)-1 do
-      ObjParams[i]:= PyString_FromString(PChar(AParams[i]));
-
-    Result:= Py_RunModuleFunction2(AModule, AFunc, ObjParams);
-
-    ////no need:
-    //for i:= Length(AParams)-1 downto 0 do
-    //  Py_DECREF(ObjParams[i]);
-  end;
-end;
 *)
 
-// By Artem:
-// https://github.com/Alexey-T/CudaText/issues/2366
-function Py_RunModuleFunction2(const AModule, AFunc: string; AParams: array of PPyObject): PPyObject;
+function Py_RunModuleFunction(const AModule,AFunc:string;AParams:array of PPyObject;const AParamNames:array of string):PPyObject;
+var
+  Module,ModuleDic,Func,Params,ParamsDic:PPyObject;
+  i,FirstNamed:integer;
+begin
+  Result:=nil;
+  with GetPythonEngine do
+  begin
+    Module:=PyImport_ImportModule(PChar(AModule));
+    if Assigned(Module) then
+    try
+      ModuleDic:=PyModule_GetDict(Module);
+      if Assigned(ModuleDic) then
+      begin
+        Func:=PyDict_GetItemString(ModuleDic,PChar(AFunc));
+        if Assigned(Func) then
+        begin
+          Params:=PyTuple_New(Length(AParams));
+          if Assigned(Params) then
+            try
+              ParamsDic:=PyDict_New();
+              if Assigned(ParamsDic) then
+                try
+                  FirstNamed:=Length(AParams)-Length(AParamNames);
+                  for i:=0 to FirstNamed-1 do
+                    if PyTuple_SetItem(Params,i,AParams[i])<>0 then
+                      RaiseError;
+                  for i:=0 to Length(AParamNames)-1 do
+                    if PyDict_SetItemString(ParamsDic,PChar(AParamNames[i]),AParams[FirstNamed+i])<>0 then
+                      RaiseError;
+                  Result:=PyObject_Call(Func,Params,ParamsDic);
+                finally
+                  Py_DECREF(ParamsDic);
+                end;
+            finally
+              Py_DECREF(Params);
+            end;
+        end;
+      end;
+    finally
+      Py_DECREF(Module);
+    end;
+  end;
+end;
+
+function Py_RunModuleFunction(const AModule, AFunc: string; AParams: array of PPyObject): PPyObject;
 var
   Module,ModuleDic,Func,Params:PPyObject;
   i:integer;
@@ -239,6 +262,7 @@ begin
         if Assigned(Func) then
         begin
           Params:=PyTuple_New(Length(AParams));
+          if Assigned(Params) then
           try
             for i:=0 to Length(AParams)-1 do
               if PyTuple_SetItem(Params,i,AParams[i])<>0 then
