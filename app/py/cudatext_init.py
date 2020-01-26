822ba4213d26fd6555dfd08843a5c436389f6fc4
Alextp <aaa5500@ya.ru>
2020-01-26 22:37:44 +0300

    cudatext_init.py: dont import cudatext

--- app/py/cudatext_init.py
+++ app/py/cudatext_init.py
@@ -1,8 +1,3 @@
 import sys
-#import os
-from cudatext import *
-
-#if os.name=='nt':
-#    os.putenv('PYTHONIOENCODING', 'UTF-8')
-
 print("Python %d.%d.%d" % sys.version_info[:3])
+#from cudatext import *
