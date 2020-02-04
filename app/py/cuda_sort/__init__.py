03241122c8f3f96877ce2f8c16df6242fc5bbc9a
Alexey-t <support@uvviewsoft.com>
2020-02-05 01:40:03 +0300

    plugin Sort: changed option names

--- app/py/cuda_sort/__init__.py
+++ app/py/cuda_sort/__init__.py
@@ -231,10 +231,10 @@ def do_dialog():
     id_offset2 = 9
     id_ok = 10
 
-    op_rev = ini_read(fn_ini, op_section, 'rev', '0')
-    op_nocase = ini_read(fn_ini, op_section, 'nocase', '0')
-    op_del_dup = ini_read(fn_ini, op_section, 'del_dup', '1')
-    op_del_sp = ini_read(fn_ini, op_section, 'del_sp', '1')
+    op_rev = ini_read(fn_ini, op_section, 'reverse', '0')
+    op_nocase = ini_read(fn_ini, op_section, 'ignore_case', '0')
+    op_del_dup = ini_read(fn_ini, op_section, 'del_dups', '1')
+    op_del_sp = ini_read(fn_ini, op_section, 'del_blanks', '1')
     op_numeric = ini_read(fn_ini, op_section, 'numeric', '0')
 
     op_offset1, op_offset2 = get_offsets()
@@ -261,10 +261,10 @@ def do_dialog():
     if btn != id_ok: return
     text = text.splitlines()
 
-    ini_write(fn_ini, op_section, 'rev', text[id_rev])
-    ini_write(fn_ini, op_section, 'nocase', text[id_nocase])
-    ini_write(fn_ini, op_section, 'del_dup', text[id_del_dup])
-    ini_write(fn_ini, op_section, 'del_sp', text[id_del_sp])
+    ini_write(fn_ini, op_section, 'reverse', text[id_rev])
+    ini_write(fn_ini, op_section, 'ignore_case', text[id_nocase])
+    ini_write(fn_ini, op_section, 'del_dups', text[id_del_dup])
+    ini_write(fn_ini, op_section, 'del_blanks', text[id_del_sp])
     ini_write(fn_ini, op_section, 'numeric', text[id_numeric])
 
     is_rev = text[id_rev]=='1'
