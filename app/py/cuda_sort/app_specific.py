f2d02bcf061c65275395d707055299df948254b7
Alexey-t <support@uvviewsoft.com>
2020-02-21 20:08:36 +0300

    plg Sort optimize

--- app/py/cuda_sort/app_specific.py
+++ app/py/cuda_sort/app_specific.py
@@ -8,14 +8,11 @@ def ed_set_text_all(lines):
     ed.set_text_all('\n'.join(lines)+'\n')
 
 def ed_get_text_all():
-    n = ed.get_line_count()
-    if ed.get_text_line(n-1)=='': n-=1
-    return [ed.get_text_line(i) for i in range(n)]    
+    return ed.get_text_all().split('\n')
 
 def ed_insert_to_lines(lines, line1, line2):
-    ed.delete(0, line1, 0, line2+1)
-    ed.insert(0, line1, '\n'.join(lines)+'\n')
-    ed.set_caret(0, line1+len(lines), 0, line1)
+    if ed.replace_lines(line1, line2, lines):
+        ed.set_caret(0, line1+len(lines), 0, line1)
 
 def ed_set_tab_title(s):
     ed.set_prop(PROP_TAB_TITLE, s)
