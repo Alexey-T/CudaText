fe77636448316e74206b22e94edcc0637893479c
Alexey <support@uvviewsoft.com>
2020-03-17 22:50:48 +0300

    add fast-sort commands, txt

--- app/py/cudatext_cmd.py
+++ app/py/cudatext_cmd.py
@@ -149,6 +149,11 @@ cCommand_ToggleMicromap = 311
 cCommand_ToggleWordWrapAlt = 312
 cCommand_ToggleUnprintedSpacesTrailing = 320
 
+cCommand_FastSort_Asc        = 330
+cCommand_FastSort_AscNoCase  = 331
+cCommand_FastSort_Desc       = 332
+cCommand_FastSort_DescNoCase = 333
+
 cCommand_ClipboardPaste = 1000
 cCommand_ClipboardPaste_Select = 1001
 cCommand_ClipboardPaste_KeepCaret = 1002
