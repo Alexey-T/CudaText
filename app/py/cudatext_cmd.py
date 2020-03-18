94a8fa77bb1083ab98abcbeef182ecdac9b133cf
Alexey <support@uvviewsoft.com>
2020-03-18 13:17:39 +0300

    txt; add cmd codes

--- app/py/cudatext_cmd.py
+++ app/py/cudatext_cmd.py
@@ -154,8 +154,9 @@ cCommand_Sort_AscNoCase  = 331
 cCommand_Sort_Desc       = 332
 cCommand_Sort_DescNoCase = 333
 
-cCommand_DeleteAdjacentBlanks = 340
-cCommand_DeleteAdjacentDups   = 341
+cCommand_DeleteAllBlanks      = 340
+cCommand_DeleteAdjacentBlanks = 341
+cCommand_DeleteAdjacentDups   = 342
 
 cCommand_ClipboardPaste = 1000
 cCommand_ClipboardPaste_Select = 1001
