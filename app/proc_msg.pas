2114b58e5c66180601e936373e3590d81be4afd3
Alexey <support@uvviewsoft.com>
2020-06-09 14:33:09 +0300

    cmdline help for -id=

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -334,9 +334,10 @@ const
       '  -nh             - Ignore saved file history'+EOL+
       '  -ns             - Ignore saved session'+EOL+
       '  -nn             - Don''t suggest to create new file if param not found'+EOL+
-      '  -w=left,top,width,height - Set position/size of app window'+EOL+
       '  -s=folder       - Specify full path of "settings" folder'+EOL+
       '  -i              - Open contents of stdin in new tab (Unix only)'+EOL+
+      '  -id=name - Set single-instance id, for groups of instances (Unix, default: cudatext.0)'+EOL+
+      '  -w=left,top,width,height - Set position/size of app window'+EOL+
       ''+EOL+
       'Filenames can be with ":line" or ":line:column" suffix to place caret.'+EOL+
       'Folder can be passed, will be opened in Project Manager plugin.'+EOL+
