c6fde221c532641df2e5a391206dbafa06822582
Alexey <support@uvviewsoft.com>
2020-06-21 22:22:43 +0300

    fix src.sh

--- setup/src.sh
+++ setup/src.sh
@@ -4,13 +4,11 @@
 mkdir ~/cudatext_build/src
 cd ~/cudatext_build/src
 curl -L https://github.com/Alexey-T/ATBinHex-Lazarus/archive/master.zip >ATBinHex.zip
-curl -L https://github.com/Alexey-T/ATFileNotif-Lazarus/archive/master.zip >ATFileNotif.zip
 curl -L https://github.com/Alexey-T/ATFlatControls/archive/master.zip >ATFlatControls.zip
 curl -L https://github.com/Alexey-T/EncConv/archive/master.zip >EncConv.zip
 curl -L https://github.com/Alexey-T/ATSynEdit/archive/master.zip >ATSynEdit.zip
 curl -L https://github.com/Alexey-T/ATSynEdit_Cmp/archive/master.zip >ATSynEdit_Cmp.zip
 curl -L https://github.com/Alexey-T/ATSynEdit_Ex/archive/master.zip >ATSynEdit_Ex.zip
-curl -L https://github.com/Alexey-T/ATTabs/archive/master.zip >ATTabs.zip
 curl -L https://github.com/Alexey-T/CudaText/archive/master.zip >CudaText.zip
 curl -L https://github.com/Alexey-T/EControl/archive/master.zip >EControl.zip
 curl -L https://github.com/Alexey-T/Python-for-Lazarus/archive/master.zip >Python.zip
