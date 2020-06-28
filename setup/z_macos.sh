4e478c1c4e27249847170fe7df8c69894ddd254b
Alexey <support@uvviewsoft.com>
2020-06-28 12:28:04 +0300

    fix z_macos.sh

--- setup/z_macos.sh
+++ setup/z_macos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 . ./cuda_ver.sh
+
 SIZE_MB=22
 TMP=~/m
 IMG=~/cudatext-macos-$cuda_ver.dmg
-#IMG=~/c.dmg
 
 dd if=/dev/zero of=$IMG bs=1M count=$SIZE_MB status=progress
 mkfs.hfsplus -v CudaText $IMG
@@ -14,4 +14,7 @@ sudo cp -a ~/cuda/cuda/app/cudatext.app $TMP/CudaText.app
 sudo mkdir $TMP/CudaText.app/Contents/MacOS
 sudo cp -a ~/cuda/cuda/app/builds/macos-cocoa/cudatext $TMP/CudaText.app/Contents/MacOS
 
-echo Must unnmount folder $TMP !
+sudo ln -s /Applications $TMP/Applications
+
+sleep 20
+sudo umount $TMP
