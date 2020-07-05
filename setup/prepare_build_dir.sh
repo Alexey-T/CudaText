ad6df1558367913125d532bd6a90be3f8bd05955
Alexey <support@uvviewsoft.com>
2020-07-05 12:27:49 +0300

    up prepare_build_dir.sh

--- setup/prepare_build_dir.sh
+++ setup/prepare_build_dir.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+CUDDIR=~/cudatext_build
+mkdir $CUDDIR
+cd $CUDDIR
 
-mkdir ~/cudatext_build
-cd ~/cudatext_build
-
-curl -L https://sourceforge.net/projects/cudatext/files/release/Windows/Python_engine/python35_x64.zip/download >py_dll_x64.zip
-curl -L https://sourceforge.net/projects/cudatext/files/release/Windows/Python_engine/python35_x32.zip/download >py_dll_x32.zip
-
-unzip py_dll_x64.zip -d ./py_dll_x64
-unzip py_dll_x32.zip -d ./py_dll_x32
+curl -L https://sourceforge.net/projects/cudatext/files/release_libs/cudatext-windows-libs.zip/download >winlibs.zip
+unzip winlibs.zip -d .
