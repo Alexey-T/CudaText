08df7fdd893fb6072d07e79282ba0862506e2a84
Alexey <support@uvviewsoft.com>
2020-07-11 02:15:24 +0300

    work on testing-plg

--- app/py/cuda_testing_finder_proc/__init__.py
+++ app/py/cuda_testing_finder_proc/__init__.py
@@ -24,7 +24,6 @@ class Command:
         if back: op+='b'
         if from_caret: op+='f'
         
-        print('finder options:', op)
         finder_proc(self.fnd, FINDER_SET_OPT, op)
         
         finder_proc(self.fnd, FINDER_SET_ED, self.edh)
@@ -35,8 +34,14 @@ class Command:
         v=dlg_proc(self.dlg, DLG_CTL_PROP_GET, name='repl')['val']
         finder_proc(self.fnd, FINDER_SET_REPTEXT, v)
         
-        print('finder: what: "'+finder_proc(self.fnd, FINDER_GET_FINDTEXT)+'", repl: "'+finder_proc(self.fnd, FINDER_GET_REPTEXT)+'"')
+        print('finder: opt: "'+op+'", find what: "'+finder_proc(self.fnd, FINDER_GET_FINDTEXT)+'", replace: "'+finder_proc(self.fnd, FINDER_GET_REPTEXT)+'"')
         
+    def do_count(self, id_dlg, id_ctl, data='', info=''):
+
+        self.apply_opt(False, False)
+        res=finder_proc(self.fnd, FINDER_COUNT)
+        print('count:', res)
+    
     def do_find(self, id_dlg, id_ctl, data='', info=''):
 
         self.apply_opt(False, False)
@@ -60,6 +65,26 @@ class Command:
         if res is not None:
             self.edtext.action(EDACTION_SHOW_POS, (res[0], res[1]))
         print('find-prev:', res)
+    
+    def do_find_all(self, id_dlg, id_ctl, data='', info=''):
+
+        self.apply_opt(False, False)
+        res=finder_proc(self.fnd, FINDER_FIND_ALL)
+        print('find-all:', res)
+    
+    def do_rep(self, id_dlg, id_ctl, data='', info=''):
+
+        self.apply_opt(False, True)
+        res=finder_proc(self.fnd, FINDER_FIND_REP, next=False, setcaret=True)
+        if res is not None:
+            self.edtext.action(EDACTION_SHOW_POS, (res[0], res[1]))
+        print('replace-next:', res)
+    
+    def do_rep_all(self, id_dlg, id_ctl, data='', info=''):
+
+        self.apply_opt(False, False)
+        res=finder_proc(self.fnd, FINDER_REP_ALL)
+        print('replace-all:', res)
     
     def run(self):
         
@@ -108,16 +133,16 @@ class Command:
         'name':'prev', 'x':105, 'y':170, 'w':100, 'h':25, 'cap':'Find prev', 'on_change': self.do_find_prev })
 
         idc=dlg_proc(idd, DLG_CTL_ADD,"button");dlg_proc(idd, DLG_CTL_PROP_SET, index=idc, prop={
-        'name':'fall', 'x':105, 'y':200, 'w':100, 'h':25, 'cap':'Find all'})
+        'name':'fall', 'x':105, 'y':200, 'w':100, 'h':25, 'cap':'Find all', 'on_change': self.do_find_all })
 
         idc=dlg_proc(idd, DLG_CTL_ADD,"button");dlg_proc(idd, DLG_CTL_PROP_SET, index=idc, prop={
-        'name':'rone', 'x':230, 'y':170, 'w':100, 'h':25, 'cap':'Replace next'})
+        'name':'rone', 'x':230, 'y':170, 'w':100, 'h':25, 'cap':'Replace next', 'on_change': self.do_rep })
 
         idc=dlg_proc(idd, DLG_CTL_ADD,"button");dlg_proc(idd, DLG_CTL_PROP_SET, index=idc, prop={
-        'name':'rall', 'x':230, 'y':200, 'w':100, 'h':25, 'cap':'Replace all'})
+        'name':'rall', 'x':230, 'y':200, 'w':100, 'h':25, 'cap':'Replace all', 'on_change': self.do_rep_all })
 
         idc=dlg_proc(idd, DLG_CTL_ADD,"button");dlg_proc(idd, DLG_CTL_PROP_SET, index=idc, prop={
-        'name':'coun', 'x':335, 'y':200, 'w':100, 'h':25, 'cap':'Count'})
+        'name':'coun', 'x':335, 'y':200, 'w':100, 'h':25, 'cap':'Count', 'on_change': self.do_count })
 
         idc=dlg_proc(idd, DLG_CTL_ADD,"label");dlg_proc(idd, DLG_CTL_PROP_SET, index=idc, prop={
         'name':'tex_', 'x':5, 'y':240, 'w':200, 'h':17, 'cap':'User text'})
