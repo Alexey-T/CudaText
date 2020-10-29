115ca75245ddc41b187ce5ec7065678e710607a4
Alexey <support@uvviewsoft.com>
2020-10-29 15:27:20 +0300

    for last atsynedit

--- app/proc_files.pas
+++ app/proc_files.pas
@@ -127,8 +127,8 @@ begin
     end;
 
     if Str.Size<=2 then exit(true);
-    if IsStreamWithUt8NoBom(Str, BufSizeKb)=TBufferUTF8State.u8sYes then exit(true);
-    if IsStreamWithUtf16NoBom(Str, BufSizeWords, IsLE) then exit(true);
+    if DetectStreamUtf8NoBom(Str, BufSizeKb)=TBufferUTF8State.u8sYes then exit(true);
+    if DetectStreamUtf16NoBom(Str, BufSizeWords, IsLE) then exit(true);
     Str.Position:= 0;
 
     GetMem(Buffer, BufSize);
