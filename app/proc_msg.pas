e434029f7e996014c3b921d6dde9e01a7de5e9e4
Alexey <support@uvviewsoft.com>
2021-01-28 00:34:35 +0300

    better msg strings about Py error

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -175,9 +175,9 @@ const
     msgCannotInitPython2: string = 'place near cudatext.exe: python3x.dll, python3x.zip, python3xdlls\*.pyd, MS VS Runtime.';
     msgCannotInitPython2b: string = '';
     {$else}
-    msgCannotInitPython2: string = ' - write "pylib'+cOptionSystemSuffix+
-                                   '" to user.json. See info in default config: "Options / Settings-default".';
-    msgCannotInitPython2b: string = ' - or use menu item "Plugins / Find Python library".';
+    msgCannotInitPython2: string = '* write "pylib'+cOptionSystemSuffix+
+                                   '" to user.json. Read about "pylib" in "Options / Settings-default".';
+    msgCannotInitPython2b: string = '* or use the menu item "Plugins / Find Python library".';
     {$endif}
   {$endif}
 
