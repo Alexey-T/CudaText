2d6d5ee10d599a80aee6ce6bc68db567040ab01c
Alexey <support@uvviewsoft.com>
2021-02-03 19:47:21 +0300

    form 'split tab'

---
+++ app/form_tabsplit.pas
@@ -0,0 +1,32 @@
+unit form_tabsplit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Spin;
+
+type
+
+  { TfmTabSplit }
+
+  TfmTabSplit = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+    RadioButton3: TRadioButton;
+    SpinEdit1: TSpinEdit;
+  private
+
+  public
+
+  end;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
