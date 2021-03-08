b7f9d7b6defac6dae69daecad86b0e987048edbe
Alexey <support@uvviewsoft.com>
2021-03-08 18:57:13 +0300

    more chars for auto_close_brackets

--- app/proc_editor.pas
+++ app/proc_editor.pas
@@ -885,7 +885,8 @@ begin
         if CharBegin='~' then CharEnd:= '~' else
          if CharBegin='*' then CharEnd:= '*' else
           if CharBegin='#' then CharEnd:= '#' else
-           exit;
+           if CharBegin='<' then CharEnd:= '>' else
+            exit;
 
   //cancel vertical selection
   Ed.DoSelect_ClearColumnBlock;
