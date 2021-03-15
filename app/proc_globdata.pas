a9bcb4abaa7309fc1e9b7dcd4709490351a0402b
Alexey <support@uvviewsoft.com>
2021-03-15 13:12:33 +0300

    add checking of AllowedThreadId in lex manager

--- app/proc_globdata.pas
+++ app/proc_globdata.pas
@@ -2639,7 +2639,9 @@ end;
 
 procedure TAppManagerThread.Execute;
 begin
+  AppManager.AllowedThreadId:= Self.ThreadID;
   AppLoadLexers;
+  AppManager.AllowedThreadId:= 0;
 end;
 
 { TAppCommandInfo }
