ef9ece0b174228f53fe2e22f8f9f471edffa5da7
Alexey <support@uvviewsoft.com>
2021-03-28 22:15:00 +0300

    prepare_cudatext_dir.sh

---
+++ setup/prepare_cudatext_dir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CUD=cudatext
+
+cd ~
+mkdir $CUD
+cd ~/$CUD
+
+git clone https://github.com/alexey-t/cudatext cuda
+git clone https://github.com/alexey-t/atbinhex-lazarus atbinhex
+git clone https://github.com/alexey-t/atflatcontrols atflatcontrols
+git clone https://github.com/alexey-t/atsynedit atsynedit
+git clone https://github.com/alexey-t/atsynedit_cmp atsynedit_cmp
+git clone https://github.com/alexey-t/atsynedit_ex atsynedit_ex
+git clone https://github.com/alexey-t/econtrol econtrol
+git clone https://github.com/alexey-t/emmet-pascal emmet
+git clone https://github.com/alexey-t/encconv encconv
+git clone https://github.com/alexey-t/python-for-lazarus python
+git clone https://github.com/bgrabitmap/bgrabitmap bgrabitmap
