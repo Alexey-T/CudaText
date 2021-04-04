c2e1024598e47ed4e737af7305c4f70fd3a32f69
Alexey <support@uvviewsoft.com>
2021-04-04 22:43:35 +0300

    fix setup scripts

--- setup/prepare_build_dir.sh
+++ setup/prepare_build_dir.sh
@@ -5,3 +5,8 @@ cd $CUDDIR
 
 curl -L https://sourceforge.net/projects/cudatext/files/release_libs/cudatext-windows-libs.zip/download >winlibs.zip
 unzip winlibs.zip -d .
+
+mkdir lib_qt
+cd lib_qt
+curl -L https://sourceforge.net/projects/cudatext/files/release_libs/libQt5Pas.zip/download >libQt5Pas.zip
+unzip libQt5Pas.zip -d .
--- setup/qt_files/readme_qt_version.txt
+++
@@ -1,7 +0,0 @@
-For CudaText Qt version, library libQt4Pas needed.
-For Ubuntu:
-
-$ sudo apt-get install libqt4pas-dev
-
-For other Linuxes, see http://wiki.freepascal.org/Qt_Interface#Quick_start_guide_for_Linux
-You need to download binary from libQt4Pas homepage.
