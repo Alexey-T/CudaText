fd0db9391350972a792fc374181bbfe493b890b6
Alexey <support@uvviewsoft.com>
2021-04-05 19:06:51 +0300

    mac

--- app/cudatext.app/Contents/Resources/py/cuda_comments/cd_comments.py
+++ app/cudatext.app/Contents/Resources/py/cuda_comments/cd_comments.py
@@ -1,10 +1,9 @@
 ''' Plugin for CudaText editor
 Authors:
-    Andrey Kvichansky    (kvichans on github.com)
+    Andrey Kvichansky (kvichans on github.com)
     Alexey Torgashin (CudaText)
 Version:
-    '0.8.8 2018-03-05'
-ToDo: (see end of file)
+    '0.8.9 2021-04-05'
 '''
 
 import  os
@@ -27,8 +26,7 @@ class Command:
     def dlg_config(self):
         save_bd_col = apx.get_opt('comment_save_column'         , False)
         at_min_bd   = apx.get_opt('comment_equal_column'        , False)
-        bUseFLn     = True
-        bSkip       = apx.get_opt('comment_move_down'           , True)
+        move_down   = apx.get_opt('comment_move_down'           , True)
 
         save_s      = _('(Line commands) Try to keep text position after (un)commenting')
         save_h      = _('Try to replace only blank(s) to keep text positions:'
@@ -59,24 +57,29 @@ class Command:
              ,dict(cid='-'   ,tp='bt'   ,t=80   ,l=610 -80-5,w=80   ,cap=_('Cancel')                                                                )
              ], dict(save=save_bd_col
                     ,vert=at_min_bd
-                    ,down=bSkip
+                    ,down=move_down
              ), focus_cid='save')
         if aid is None or aid=='-': return
         if vals['save'] != save_bd_col: apx.set_opt('comment_save_column'       , vals['save'])
         if vals['vert'] != at_min_bd:   apx.set_opt('comment_equal_column'      , vals['vert'])
-        if vals['down'] != bSkip:       apx.set_opt('comment_move_down'         , vals['down'])
+        if vals['down'] != move_down:   apx.set_opt('comment_move_down'         , vals['down'])
        #def dlg_config
 
     def cmt_toggle_line_1st(self):
         return self._cmt_toggle_line('bgn', '1st')
+
     def cmt_add_line_1st(self):
         return self._cmt_toggle_line('add', '1st')
+
     def cmt_toggle_line_body(self):
         return self._cmt_toggle_line('bgn', 'bod')
+
     def cmt_add_line_body(self):
         return self._cmt_toggle_line('add', 'bod')
+
     def cmt_del_line(self):
         return self._cmt_toggle_line('del')
+
     def _cmt_toggle_line(self, cmt_act, cmt_type='', ed_=ed):
         ''' Add/Remove full line comment
             Params
@@ -88,28 +91,32 @@ class Command:
         '''
 #       if not apx._check_API('1.0.108'):    return
         lex         = ed_.get_prop(app.PROP_LEXER_CARET)
+        if not lex:
+            return
         prop        = app.lexer_proc(app.LEXER_GET_PROP, lex)
-        cmt_sgn     = prop['c_line'] if prop else None
+        if not prop:
+            return
+        cmt_sgn     = prop['c_line']
         pass;                  #log('cmt_type, lex, cmt_sgn={}', (cmt_type, lex, cmt_sgn))
         if not cmt_sgn:
-            return app.msg_status(f(_('No line comment for lexer "{}"'), lex))
+            return app.msg_status(f(_('Lexer "{}" don\'t support "line comments"'), lex))
         # Analize
-        bEmpSel     = False
+        empty_sel   = False
         rWrks       = []
-        bUseRepLns  = app.app_api_version()>='1.0.177'
-        y1,y2,lines = (-1, -1, []) if bUseRepLns else (None, None, None) # To use API replace_lines
+        use_rep_lines = True # use API replace_lines()
+        y1,y2,lines = (-1, -1, []) if use_rep_lines else (None, None, None)
         pass;                  #LOG and log('ed_.get_sel_mode(),app.SEL_NORMAL,app.SEL_COLUMN={}', (ed_.get_sel_mode(),app.SEL_NORMAL,app.SEL_COLUMN))
         crts        = ed_.get_carets()
         if False:pass
         elif ed_.get_sel_mode() == app.SEL_NORMAL:
-            bEmpSel     = 1==len(crts) and -1==crts[0][3]
+            empty_sel     = 1==len(crts) and -1==crts[0][3]
             for (cCrt, rCrt ,cEnd, rEnd) in crts:
                 (rCrtMin
                 ,rCrtMax)   = apx.minmax(rCrt, rEnd if -1!=rEnd else rCrt)
                 if -1!=rEnd and rCrt>rEnd and 0==cCrt:
                     rCrtMax = rCrtMax-1    # For direct section along left bound
                 rWrks      += list(range(rCrtMin, rCrtMax+1))
-            bUseRepLns  = bUseRepLns and 1==len(crts)
+            use_rep_lines  = use_rep_lines and 1==len(crts)
         elif ed_.get_sel_mode() == app.SEL_COLUMN:
             (cBgn
             ,rSelBgn
@@ -119,7 +126,7 @@ class Command:
         if not rWrks:
             rWrks       = [crts[0][1]]
         pass;                  #log('rWrks={}', (rWrks))
-        y1,y2       = (rWrks[0],rWrks[-1]) if bUseRepLns else (y1,y2)
+        y1,y2       = (rWrks[0],rWrks[-1]) if use_rep_lines else (y1,y2)
         pass;                  #LOG and log('y1,y2,lines={}', (y1,y2,lines))
         do_uncmt    = ed_.get_text_line(rWrks[0]).lstrip().startswith(cmt_sgn) \
                         if cmt_act=='bgn' else \
@@ -130,7 +137,7 @@ class Command:
         save_bd_col = apx.get_opt('comment_save_column' , False)
         at_min_bd   = apx.get_opt('comment_equal_column', False)
         col_min_bd  = 1000 # infinity
-        bColKept = False # plugin applied the "Try to keep text position"
+        col_kept    = False # plugin applied the "Try to keep text position"
         if at_min_bd:
             for rWrk in rWrks:
                 line        = ed_.get_text_line(rWrk)
@@ -150,7 +157,7 @@ class Command:
                 # Uncomment!
                 if not line[pos_body:].startswith(cmt_sgn):
                     # Already no comment
-                    if bUseRepLns:
+                    if use_rep_lines:
                         lines += [line]
                     continue    #for rWrk
                 if False:pass
@@ -160,25 +167,25 @@ class Command:
                                       ' '==line[pos_body+len(cmt_sgn)]):
                     # Before or after cmt_sgn must be blank
                     line = line.replace(cmt_sgn, blnks4cmt, 1)
-                    bColKept = True
+                    col_kept = True
                 else:
                     line = line.replace(cmt_sgn, ''       , 1)
             else:
                 # Comment!
                 if cmt_type=='bod' and line[pos_body:].startswith(cmt_sgn):
                     # Body comment already sets - willnot double it
-                    if bUseRepLns:
+                    if use_rep_lines:
                         lines += [line]
                     continue    #for rWrk
                 if False:pass
                 elif cmt_type=='1st' and save_bd_col and line.startswith(blnks4cmt) :
                     line = line.replace(blnks4cmt, cmt_sgn, 1)
-                    bColKept = True
+                    col_kept = True
                #elif cmt_type=='1st' and save_bd_col #  !line.startswith(blnks4cmt) :
                 elif cmt_type=='1st':#  !save_bd_col
                     line = cmt_sgn+line
                 elif cmt_type=='bod' and save_bd_col and line.startswith(blnks4cmt):
-                    bColKept = True
+                    col_kept = True
                     pos_cmnt = col_min_bd if at_min_bd else pos_body
                     pass;          #LOG and log('pos_cmnt={}', (pos_cmnt))
                     if pos_cmnt>=len(cmt_sgn):
@@ -195,25 +202,25 @@ class Command:
                    #line = line[:pos_body]             +cmt_sgn+line[pos_body:]
 
             pass;              #LOG and log('new line={}', (line))
-            if bUseRepLns:
+            if use_rep_lines:
                 lines += [line]
             else:
                 pass;           log('line={}',(line))
                 ed_.set_text_line(rWrk, line)
             #for rWrk
-        if bUseRepLns:
+        if use_rep_lines:
             pass;              #log('y1, y2, len(lines), lines={}',(y1, y2, len(lines), lines))
             if y1==y2:
                 ed_.set_text_line(y1, lines[0])
             else:
                 ed_.replace_lines(y1, y2, lines)
         # move caret down
-        bSkip    = apx.get_opt('comment_move_down', True)
-        if bEmpSel and bSkip:
+        move_down = apx.get_opt('comment_move_down', True)
+        if empty_sel and move_down:
             (cCrt, rCrt, cEnd, rEnd)    = crts[0]
             apx._move_caret_down(cCrt, rCrt)
         # shift caret horizontally if it's on the same line
-        if not bSkip and bEmpSel and not bColKept:
+        if not move_down and empty_sel and not col_kept:
             dx = len(cmt_sgn)
             if do_uncmt:
                 dx = -dx
@@ -231,28 +238,27 @@ class Command:
         ,bOnlyLn)=self._get_cmt_pair(lex)
         if not bgn_sgn:
             return app.msg_status(f(_('No stream comment for lexer "{}"'), lex))
-        bUseFLn = True
         crts    = ed.get_carets()
         pass;                  #LOG and log('lex, get_carets()={}', (lex, crts))
         pass;                  #LOG and log('(bgn_sgn,end_sgn),bOnlyLn,bUseFLn={}', ((bgn_sgn,end_sgn),bOnlyLn,bUseFLn))
         for icrt, (cCrt, rCrt, cEnd, rEnd) in enumerate(crts):
             pass;              #LOG and log('(cCrt, rCrt), (cEnd, rEnd)={}', ((cCrt, rCrt), (cEnd, rEnd)))
-            bEmpSel     = -1==rEnd
+            empty_sel     = -1==rEnd
             bDrtSel     = -1==rEnd or (rCrt, cCrt)>(rEnd, cEnd)
             bEntireLn   = (rEnd>=0) and (cEnd==0) and (cCrt==0)
             bEntireLn1  = bEntireLn and abs(rEnd-rCrt)==1
             bEntireLn2  = bEntireLn and abs(rEnd-rCrt)>1
             if False:pass
-            elif bEmpSel and (bUseFLn or bOnlyLn):
+            elif empty_sel:
                 # Use full line
                 line        = ed.get_text_line(rCrt)
                 (cTx1, rTx1), (cTx2, rTx2) = (0, rCrt), (len(line), rCrt)
-            elif bOnlyLn: # and not bEmpSel
+            elif bOnlyLn: # and not empty_sel
                 # Only full lines
                 rTx1, rTx2  = apx.minmax(rCrt, rEnd)
                 line    = ed.get_text_line(rTx2)
                 (cTx1, rTx1), (cTx2, rTx2) = (0, rTx1), (len(line), rTx2)
-            elif bEmpSel: # and not bUseFLn and not bOnlyLn
+            elif empty_sel: # and not bUseFLn and not bOnlyLn
                 continue
             else:
                 (rTx1, cTx1), (rTx2, cTx2) = apx.minmax((rCrt, cCrt), (rEnd, cEnd))
@@ -341,9 +347,9 @@ class Command:
             else:
                 ed.set_caret(cNSel1, rNSel1, cNSel2, rNSel2, app.CARET_SET_INDEX+icrt)
            #for icrt
-        bSkip    = apx.get_opt('comment_move_down', True)
+        move_down = apx.get_opt('comment_move_down', True)
         if False:pass
-        elif 1==len(crts) and bEmpSel and bUseFLn and bSkip:
+        elif 1==len(crts) and empty_sel and move_down:
             apx._move_caret_down(cCrt, rCrt)
             if bOnlyLn and not do_uncmt:
                 crt=ed.get_carets()[0]; apx._move_caret_down(crt[0], crt[1])
@@ -371,9 +377,3 @@ class Command:
             self.pair4lex[lex] = (pair, only_ln)
         return self.pair4lex[lex]
        #def _get_cmt_pair
-
-
-'''
-ToDo
-[ ][kv-kv][13sep16] Start
-'''
