706337da4a86ef4a07336a28fdbad52c344b43f5
Alexey <support@uvviewsoft.com>
2021-06-29 11:39:49 +0300

    fix src.sh

--- setup/src.sh
+++ setup/src.sh
@@ -9,10 +9,8 @@ curl -L https://github.com/Alexey-T/EncConv/archive/master.zip >EncConv.zip
 curl -L https://github.com/Alexey-T/ATSynEdit/archive/master.zip >ATSynEdit.zip
 curl -L https://github.com/Alexey-T/ATSynEdit_Cmp/archive/master.zip >ATSynEdit_Cmp.zip
 curl -L https://github.com/Alexey-T/ATSynEdit_Ex/archive/master.zip >ATSynEdit_Ex.zip
-#curl -L https://github.com/Alexey-T/ATSynEdit_Ex/archive/v2.zip >ATSynEdit_Ex-v2.zip
 curl -L https://github.com/Alexey-T/CudaText/archive/master.zip >CudaText.zip
 curl -L https://github.com/Alexey-T/EControl/archive/master.zip >EControl.zip
-#curl -L https://github.com/Alexey-T/EControl/archive/v2.zip >EControl-v2.zip
 curl -L https://github.com/Alexey-T/Python-for-Lazarus/archive/master.zip >Python.zip
 curl -L https://github.com/Alexey-T/Emmet-Pascal/archive/master.zip >Emmet.zip
 curl -L https://github.com/dinkumoil/cuda_shell_extension/archive/master.zip >ShellExtension.zip
