c5a9f99e0dd1ce394974db7530bcbc2315175900
Alexey <support@uvviewsoft.com>
2021-07-15 16:21:59 +0300

    Emmet plg, https://github.com/Alexey-T/CudaText/issues/3554

--- app/cudatext.app/Contents/Resources/py/cuda_emmet/proc_snip_insert.py
+++ app/cudatext.app/Contents/Resources/py/cuda_emmet/proc_snip_insert.py
@@ -5,21 +5,21 @@ import cudatext_cmd
 def insert_snip_into_editor(ed, snip_lines):
     items = list(snip_lines) #copy list value
     if not items: return
-    
+
     carets = ed.get_carets()
     if len(carets)!=1: return
     x0, y0, x1, y1 = carets[0]
 
     tab_spaces = ed.get_prop(ct.PROP_TAB_SPACES)
     tab_size = ed.get_prop(ct.PROP_TAB_SIZE)
-    
+
     #apply indent to lines from second
     x_col, y_col = ed.convert(ct.CONVERT_CHAR_TO_COL, x0, y0)
     indent = ' '*x_col
-    
+
     if not tab_spaces:
         indent = indent.replace(' '*tab_size, '\t')
-    
+
     for i in range(1, len(items)):
         items[i] = indent+items[i]
 
@@ -35,33 +35,36 @@ def insert_snip_into_editor(ed, snip_lines):
         while True:
             digit = 0
             deftext = ''
-            
+
             n = s.find('${')
             if n<0: break
-            if n+3>=len(s): break
-            if not s[n+3] in ':}': break
+            n_close = s.find('}', n)
+            if n_close<0: break
+            n_colon = s.find(':', n)
+
+            digit_end = n_close
+            if n_colon>=0:
+                digit_end = min(n_close, n_colon)
+
             try:
-                digit = int(s[n+2])
+                digit = int(s[n+2:digit_end])
             except:
                 break
-                
-            #text in tabstop    
-            if s[n+3]==':':
-                deftext = s[n+4:]
-                nn = deftext.find('}')
-                if nn<0: break
-                deftext = deftext[:nn] 
-                s = s[:n]+deftext+s[n+5+nn:]
+
+            #text in tabstop
+            if n_colon>=0:
+                deftext = s[n_colon+1:n_close]
+                s = s[:n]+deftext+s[n_close+1:]
             else:
-                s = s[:n]+s[n+4:]
-                
+                s = s[:n]+s[n_close+1:]
+
             stops += [(digit, deftext, index, n)]
             items[index] = s
-    #print('tabstops', stops)        
-    
-    #insert    
+    #print('tabstops', stops)
+
+    #insert
     ed.insert(x0, y0, '\n'.join(items))
-    
+
     #place markers
     mark_placed = False
     ed.markers(ct.MARKERS_DELETE_ALL)
@@ -77,7 +80,7 @@ def insert_snip_into_editor(ed, snip_lines):
                 deftext = stop[1]
                 ed.markers(ct.MARKERS_ADD, pos_x, pos_y, digit, len(deftext))
                 mark_placed = True
-    
+
     if mark_placed:
         ed.set_prop(ct.PROP_TAB_COLLECT_MARKERS, '1')
         ed.cmd(cudatext_cmd.cmd_Markers_GotoLastAndDelete)
