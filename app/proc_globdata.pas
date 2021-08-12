7d09f675ddefad0e4accf9a2afd31d938a2ca4d7
Alexey <support@uvviewsoft.com>
2021-08-12 22:50:26 +0300

    cln

--- app/proc_globdata.pas
+++ app/proc_globdata.pas
@@ -678,7 +678,6 @@ var
   AppFile_OptionsUser: string;
   AppFile_History: string;
   AppFile_HistoryFiles: string;
-  AppFile_HistoryBookmarks: string;
   AppFile_Hotkeys: string;
   AppFile_PluginsIni: string;
 
@@ -1377,7 +1376,6 @@ begin
   AppFile_OptionsUser:= AppDir_Settings+DirectorySeparator+'user.json';
   AppFile_History:= AppDir_Settings+DirectorySeparator+'history.json';
   AppFile_HistoryFiles:= AppDir_Settings+DirectorySeparator+'history files.json';
-  AppFile_HistoryBookmarks:= AppDir_Settings+DirectorySeparator+'history bk.json';
   AppFile_Hotkeys:= AppDir_Settings+DirectorySeparator+'keys.json';
   AppFile_PluginsIni:= AppDir_Settings+DirectorySeparator+'plugins.ini';
 
