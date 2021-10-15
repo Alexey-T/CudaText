f210b31674d9aba424d161b0653e64276f393f8f
Alexey <support@uvviewsoft.com>
2021-10-15 21:39:07 +0300

    better build script for Solaris, make .zip

--- setup/z_solaris_x64.sh
+++ setup/z_solaris_x64.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
-./unix_common.sh solaris gtk2 amd64 builds/solaris-x64-gtk2 ~/cudatext_build
+
+. ./cuda_ver.sh
+os=solaris
+widgets=gtk2
+cpu=amd64
+
+outdir=~/cudatext_build
+outdir_tmp=$outdir/tmp_solaris
+
+# make .tar.xz
+
+./unix_common.sh $os $widgets $cpu builds/solaris-x64-gtk2 $outdir
+
+
+# make .zip too
+
+mkdir -p $outdir_tmp
+file_xz=$outdir/cudatext-$os-$widgets-$cpu-$cuda_ver.tar.xz
+file_zip=$outdir/cudatext-$os-$widgets-$cpu-$cuda_ver.zip
+
+tar -xf $file_xz -C $outdir_tmp
+cd $outdir_tmp
+zip -r $file_zip ./*
+cd ..
+rm -rf $outdir_tmp
