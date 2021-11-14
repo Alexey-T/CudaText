557d85b410a10bd2cc6a1258a7e7d15924c3fe2e
Alexey <support@uvviewsoft.com>
2021-11-14 19:46:48 +0300

    add script z_linux_arm_qt5

--- setup/z_linux_arm_gtk2.sh
+++ setup/z_linux_arm_gtk2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./unix_common.sh linux gtk2 arm builds/linux-arm ~/cudatext_build
+./unix_common.sh linux gtk2 arm builds/linux-arm-gtk2 ~/cudatext_build
---
+++ setup/z_linux_arm_qt5.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./unix_common.sh linux qt5 arm builds/linux-arm-qt5 ~/cudatext_build
