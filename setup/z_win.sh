d246026fb1d79ad7478aa946f5ddb6d433c3ba21
Alexey <support@uvviewsoft.com>
2021-12-01 22:03:05 +0300

    rework Win build-scripts

--- setup/z_win.sh
+++ setup/z_win.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir ~/cudatext_build
 ./win_common.sh x64 ~/cudatext_build ../app/builds/win64 ~/cudatext_build/lib_win_x64
+./win_common.sh x32 ~/cudatext_build ../app/builds/win32 ~/cudatext_build/lib_win_x32
--- setup/z_win_x32.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir ~/cudatext_build
-./win_common.sh x32 ~/cudatext_build ../app/builds/win32 ~/cudatext_build/lib_win_x32
