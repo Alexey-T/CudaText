8c1a00cd57a3af249fdb2a3741bf66dbca85126c
Alexey <support@uvviewsoft.com>
2022-03-14 11:48:15 +0300

    fix build-script for win32

--- setup/z_win.sh
+++ setup/z_win.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./win_common.sh x64 ~/cudatext_build ../app/builds/win64 ~/cudatext_build/lib_win_x64
-./win_common.sh x32 ~/cudatext_build ../app/builds/win32 ~/cudatext_build/lib_win_x32
+./win_common.sh amd64 ~/cudatext_build ../app/builds/win64 ~/cudatext_build/lib_win_x64
+./win_common.sh i386 ~/cudatext_build ../app/builds/win32 ~/cudatext_build/lib_win_x32
