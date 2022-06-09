4825d40247552500b860d5052787303b0755b6a4
Alexey <support@uvviewsoft.com>
2022-06-09 06:35:59 +0300

    cmt

--- comp/win32titlestyler.pas
+++ comp/win32titlestyler.pas
@@ -62,7 +62,7 @@ begin
       AForm.Width:= AForm.Width-1;
     end;
       {
-      //this doesn't help: titlebar is not repainted, and maximized state is lost
+      //this doesn't help: titlebar is not repainted after returning from full-screen, and maximized state is lost
       SetWindowPos(AForm.Handle, 0,
         0, 0, 0, 0,
         SWP_NOMOVE or SWP_NOSIZE or SWP_NOZORDER or SWP_NOSENDCHANGING or SWP_DRAWFRAME);
