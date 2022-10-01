44fe159cd3216ca49bb96771b50c28c93287b8af
Alexey <support@uvviewsoft.com>
2022-10-01 16:58:05 +0300

    revert

--- app/cudatext.app/Contents/Resources/py/sys/requests/compat.py
+++ app/cudatext.app/Contents/Resources/py/sys/requests/compat.py
@@ -36,21 +36,10 @@ try:
 except ImportError:
     import json
 
-# copy/paste from https://github.com/python/cpython/blob/3.10/Lib/json/decoder.py
-class JSONDecodeError(ValueError):
-    def __init__(self, msg, doc, pos):
-        lineno = doc.count('\n', 0, pos) + 1
-        colno = pos - doc.rfind('\n', 0, pos)
-        errmsg = '%s: line %d column %d (char %d)' % (msg, lineno, colno, pos)
-        ValueError.__init__(self, errmsg)
-        self.msg = msg
-        self.doc = doc
-        self.pos = pos
-        self.lineno = lineno
-        self.colno = colno
-
-    def __reduce__(self):
-        return self.__class__, (self.msg, self.doc, self.pos)
+if has_simplejson:
+    from simplejson import JSONDecodeError
+else:
+    from json import JSONDecodeError
 
 # Keep OrderedDict for backwards compatibility.
 from collections import OrderedDict
