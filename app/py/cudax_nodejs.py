e6606434f487391dbd5540256f77e59880f7e80f
Alexey <support@uvviewsoft.com>
2022-10-02 11:41:28 +0300

    better cudax_nodejs

--- app/py/cudax_nodejs.py
+++ app/py/cudax_nodejs.py
@@ -1,26 +1,9 @@
 import os
 import platform
 import subprocess
+from shutil import which
 
 MSG_CANNOT_RUN_NODE = "Cannot run Node.js. Make sure it's in your PATH."
-
-# https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
-def which(program):
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
 
 #
 # Linux: it may be "node" or "nodejs"
