bac0ba48496a7c5e4853047666a30e44e0419728
Alexey <support@uvviewsoft.com>
2022-10-31 15:12:17 +0300

    improve prev fix

--- app/formfind.pas
+++ app/formfind.pas
@@ -1362,6 +1362,10 @@ var
   Ed: TATSynEdit;
   bEnabled: boolean;
 begin
+  //form can be already closed ('select all' closes it);
+  //and we must avoid UpdateHiAll etc.
+  if not Visible then exit;
+
   cPadding:= ATEditorScale(4);
   bEnabled:= Self.Enabled;
 
@@ -1415,10 +1419,6 @@ begin
   edRep.Left:= cPadding;
   edFind.OptTextHint:= msgFindHint_InputFind;
   edRep.OptTextHint:= msgFindHint_InputRep;
-
-  //form can be already closed ('select all' closes it);
-  //and we must avoid UpdateHiAll etc.
-  if not Visible then exit;
 
   UpdateButtonBold;
   UpdateFormHeight;
