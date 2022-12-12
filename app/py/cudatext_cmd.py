019693e275bb3af8993e59ee59c4811980f88175
Alexey <support@uvviewsoft.com>
2022-12-13 02:22:54 +0300

    py cmds codes

--- app/py/cudatext_cmd.py
+++ app/py/cudatext_cmd.py
@@ -503,6 +503,7 @@ cmd_Encoding_iso13_NoReload     = 2745
 cmd_Encoding_iso7_NoReload      = 2746
 cmd_Encoding_iso3_NoReload      = 2747
 cmd_Encoding_iso4_NoReload      = 2748
+cmd_Encoding_cp861_NoReload     = 2749
 
 cmd_Encoding_ansi_Reload      = 2750
 cmd_Encoding_utf8bom_Reload   = 2751
@@ -543,6 +544,7 @@ cmd_Encoding_iso13_Reload     = 2785
 cmd_Encoding_iso7_Reload      = 2786
 cmd_Encoding_iso3_Reload      = 2787
 cmd_Encoding_iso4_Reload      = 2788
+cmd_Encoding_cp861_Reload     = 2789
 
 cmd_Markers_SelectToCaret      = 2798
 cmd_Markers_DeleteToCaret      = 2799
