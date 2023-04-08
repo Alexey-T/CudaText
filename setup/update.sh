4669c9c999ae4869820b3aa9dbdea2f0a1899653
ziggy <z4ziggy@users.noreply.github.com>
2023-04-08 21:47:38 +0300

    add: commit submodules changes to master

--- setup/update.sh
+++ setup/update.sh
@@ -3,7 +3,15 @@
 # update submodules
 git submodule foreach git pull origin master
 
+# list git submodules changed
+for r in `git submodule status | grep '^+' | cut -f2 -d' '`; do
+	# commit to master with commit summary
+	git commit $r -m "Merge branch 'master' of `git -C $r remote get-url origin`
+`git submodule summary $r | tail -n +2`"
+done
+
 # update upstream
 git pull upstream master
 
+# run the build script
 bash "$(dirname -- "${BASH_SOURCE[0]}")"/build.sh
