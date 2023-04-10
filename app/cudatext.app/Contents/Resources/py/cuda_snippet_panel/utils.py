91a85418794e60914201c727858fcfc574ae1b77
Alexey <support@uvviewsoft.com>
2023-04-10 20:06:21 +0300

    fix in cuda_snippet_panel

--- app/cudatext.app/Contents/Resources/py/cuda_snippet_panel/utils.py
+++ app/cudatext.app/Contents/Resources/py/cuda_snippet_panel/utils.py
@@ -15,9 +15,9 @@ https://stackoverflow.com/questions/436220/determine-the-encoding-of-text-in-pyt
 def bom_type(file):
     """
     returns file encoding string for open() function
-    EXAMPLE:
-        bom = bomtype(file)
-        open(file, encoding=bom, errors='ignore')
+    example:
+        bom = bom_type(f)
+        open(f, encoding=bom, errors='ignore')
     """
 
     f = open(file, 'rb')
@@ -29,11 +29,12 @@ def bom_type(file):
 
     # Python automatically detects endianness if utf-16 bom is present
     # write endianness generally determined by endianness of CPU
-    if ((b[0:2] == b'\xfe\xff') or (b[0:2] == b'\xff\xfe')):
+    if (b[0:2] == b'\xfe\xff') or \
+       (b[0:2] == b'\xff\xfe'):
         return "utf16"
 
-    if ((b[0:5] == b'\xfe\xff\x00\x00') 
-              or (b[0:5] == b'\x00\x00\xff\xfe')):
+    if (b[0:4] == b'\xfe\xff\x00\x00') or \
+       (b[0:4] == b'\x00\x00\xff\xfe'):
         return "utf32"
 
     # If BOM is not provided, then assume its the codepage
@@ -42,8 +43,7 @@ def bom_type(file):
     # For the United States its: cp1252
 
 
-def open_read(file):
+def open_read(f):
 
-    bom = bom_type(file)
-    return open(file, 'r', encoding=bom, errors='ignore').read()
-    
+    bom = bom_type(f)
+    return open(f, 'r', encoding=bom, errors='ignore').read()
