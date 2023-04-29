8c0050663218d683cb2d4382d9afb84af4702cf4
Alexey <support@uvviewsoft.com>
2023-04-29 12:07:10 +0300

    fixing https://github.com/Alexey-T/CudaText/issues/5031#issuecomment-1527910561

--- app/cudatext.app/Contents/Resources/py/cuda_prefs/dlg.py
+++ app/cudatext.app/Contents/Resources/py/cuda_prefs/dlg.py
@@ -1133,7 +1133,12 @@ class DialogMK2:
         # set scope
         if not is_opt_modified: # do not switch scope automatically if value of that scope was not changed yet.
             new_scope = 'u' # user scope
-            active_scoped_val = ( new_scope, self.optman.get_opt_scope_value(self._cur_opt, new_scope, is_ui=False) )
+            active_scoped_val = (
+                new_scope,
+                self.optman.get_opt_scope_value(self._cur_opt, new_scope, is_ui=False)
+                # if user scope has None value, get default
+                or self.optman.get_opt_scope_value(self._cur_opt, 'def', is_ui=False)
+            )
         new_scope_name = self._scope_captions[new_scope]
         with ignore_edit(self.h, self.scope_ed):
             self.scope_ed.set_text_all(new_scope_name)
