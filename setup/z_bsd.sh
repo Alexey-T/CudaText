ef401fb50af7a5fb2869ef6f02b5d2119393dcf0
Alexey <support@uvviewsoft.com>
2023-08-23 16:41:45 +0300

    up setup-scripts

--- setup/z_bsd.sh
+++ setup/z_bsd.sh
@@ -3,5 +3,9 @@
 ./unix_common.sh freebsd gtk2 i386 builds/freebsd-x32-gtk2 ~/cudatext_build
 
 ./unix_common.sh netbsd gtk2 amd64 builds/netbsd-x64-gtk2 ~/cudatext_build
+./unix_common.sh netbsd gtk2 i386 builds/netbsd-x32-gtk2 ~/cudatext_build
+
 ./unix_common.sh openbsd gtk2 amd64 builds/openbsd-x64-gtk2 ~/cudatext_build
+./unix_common.sh openbsd gtk2 i386 builds/openbsd-x32-gtk2 ~/cudatext_build
+
 ./unix_common.sh dragonflybsd gtk2 amd64 builds/dragonflybsd-x64-gtk2 ~/cudatext_build
