be8ab4640c3c60eb2cb0255ec26477eb8909c8bd
Alexey <support@uvviewsoft.com>
2023-09-04 20:13:19 +0300

    better debug writeln

--- app/proc_globdata.pas
+++ app/proc_globdata.pas
@@ -1588,11 +1588,11 @@ begin
   if not DirectoryExistsUTF8(AppDir_Settings) then
     if not CreateDirUTF8(AppDir_Settings) then
     begin
-      MsgStdout(msgCannotCreateDir+' '+AppDir_Settings, true);
-      MsgStdout('* Variable "HOME": "'+GetEnvironmentVariable('HOME')+'"');
-      MsgStdout('* Variable "XDG_CONFIG_HOME": "'+GetEnvironmentVariable('XDG_CONFIG_HOME')+'"');
-      MsgStdout('* OpDirLocal: '+OpDirLocal);
-      MsgStdout('* OpDirExe: '+OpDirExe);
+      MsgStdout('Cannot create "settings" dir: '+AppDir_Settings, true);
+      MsgStdout('  Variable "HOME": "'+GetEnvironmentVariable('HOME')+'"');
+      MsgStdout('  Variable "XDG_CONFIG_HOME": "'+GetEnvironmentVariable('XDG_CONFIG_HOME')+'"');
+      MsgStdout('  OpDirLocal: '+OpDirLocal);
+      MsgStdout('  OpDirExe: '+OpDirExe);
       Halt;
     end;
 
