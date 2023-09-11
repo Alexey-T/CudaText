589e9a71d9a965ad42b14455d5673f2b7ca26237
Alexey <support@uvviewsoft.com>
2023-09-11 16:20:20 +0300

    unix scripts: fix

--- setup/unix_common.sh
+++ setup/unix_common.sh
@@ -9,28 +9,29 @@ exedir=$4
 outdir=$5
 
 mkdir -p $outdir
-zipfile2=$outdir/cudatext-$os-$widgets-$cpu-$cuda_ver.tar
-zipfile=$outdir/_cud.tar
+tarfile2=$outdir/cudatext-$os-$widgets-$cpu-$cuda_ver.tar
+tarfile=$outdir/_cud.tar
 
 rm $outdir/cudatext-$os-$widgets-$cpu-*.xz
-rm $zipfile
+rm $tarfile
 
-tar --exclude=*.pyc -cf $zipfile readme data settings_default py/*.py py/cuda_addonman py/cuda_project_man py/cuda_tabs_list py/cuda_make_plugin py/cuda_insert_time py/sys py/cuda_comments py/cuda_new_file py/cuda_palette py/cuda_prefs py/cuda_multi_installer py/cuda_sort py/cuda_snippet_panel py/cuda_lexer_detecter
+tar --exclude=*.pyc -cf $tarfile readme data settings_default py/*.py py/cuda_addonman py/cuda_project_man py/cuda_tabs_list py/cuda_make_plugin py/cuda_insert_time py/sys py/cuda_comments py/cuda_new_file py/cuda_palette py/cuda_prefs py/cuda_multi_installer py/cuda_sort py/cuda_snippet_panel py/cuda_lexer_detecter
 
 cd ../setup/debfiles
-tar -rf $zipfile cudatext-512.png
+tar -rf $tarfile cudatext-512.png
 
 cd ../../app
 cd $exedir
-tar -rf $zipfile cudatext
+tar -rf $tarfile cudatext
 
 cd $outdir
 rm -rf cudatext/
 mkdir cudatext
 cd cudatext
-tar -xf $zipfile
+tar -xf $tarfile
 cd ..
-tar -rf $zipfile2 cudatext/
-xz -z cudatext
+tar -rf $tarfile2 cudatext/
+rm $tarfile
 rm -rf cudatext/
-rm $zipfile
+
+xz -z $tarfile2
