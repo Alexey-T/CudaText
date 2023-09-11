45c48fbc5c4d281cdf5594d7a948392ff08fa444
Alexey <support@uvviewsoft.com>
2023-09-11 15:54:39 +0300

    windows scripts: pack into dir 'cudatext'

--- setup/win_common.sh
+++ setup/win_common.sh
@@ -7,12 +7,15 @@ outdir=$2
 exedir=$3
 pydir=$4
 
-zipname=cudatext-windows-$cpu-$cuda_ver.zip
+zipname2=cudatext-windows-$cpu-$cuda_ver.zip
+zipname=_cud.zip
 mkdir -p $outdir
 mkdir $outdir/src
 mkdir $pydir
 
 rm $outdir/$zipname
+rm $outdir/$zipname2
+
 zip -r -x*.pyc $outdir/$zipname data readme settings_default *.manifest py/*.py py/cuda_addonman py/cuda_project_man py/cuda_tabs_list py/cuda_make_plugin py/cuda_insert_time py/sys py/cuda_comments py/cuda_new_file py/cuda_palette py/cuda_prefs py/cuda_multi_installer py/cuda_sort py/cuda_snippet_panel py/cuda_lexer_detecter
 
 # Take exe
@@ -21,3 +24,9 @@ zip $outdir/$zipname cudatext.exe
 
 cd $pydir
 zip -r $outdir/$zipname *
+
+cd $outdir
+rm -rf cudatext/
+unzip -d cudatext $zipname
+zip -r $outdir/$zipname2 cudatext
+rm $outdir/$zipname
