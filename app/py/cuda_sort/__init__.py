2f4887103ab494a4bbb6d77ccd2f0b26d31c8ac8
Alexey <support@uvviewsoft.com>
2024-01-04 12:31:51 +0300

    Sort plg: fix dlg string

--- app/py/cuda_sort/__init__.py
+++ app/py/cuda_sort/__init__.py
@@ -267,7 +267,7 @@ def do_dialog():
       c1.join(['type=check', 'pos=6,30,300,0', 'cap='+_('&Ignore case'), 'val='+op_nocase]),
       c1.join(['type=check', 'pos=6,54,300,0', 'cap='+_('Delete d&uplicate lines'), 'val='+op_del_dup]),
       c1.join(['type=check', 'pos=6,78,300,0', 'cap='+_('Delete &blank lines'), 'val='+op_del_sp]),
-      c1.join(['type=check', 'pos=6,102,300,0', 'cap='+_('Numeric (treat beginning as number)'), 'val='+op_numeric]),
+      c1.join(['type=check', 'pos=6,102,300,0', 'cap='+_('Numeric (treat line as [text1, number, text2])'), 'val='+op_numeric]),
       c1.join(['type=label', 'pos=6,130,300,0', 'cap='+_('Sort only by substring, offsets 0-based:')]),
       c1.join(['type=label', 'pos=30,152,130,0', 'cap='+_('&From:')]),
       c1.join(['type=spinedit', 'pos=30,170,110,0', 'ex0=-1', 'ex1=5000', 'ex2=1', 'val='+str(op_offset1)]),
