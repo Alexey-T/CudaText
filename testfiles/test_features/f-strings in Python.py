2d663bad3d547211bd149e5540ad6b8658e19d71
Alexey <support@uvviewsoft.com>
2024-04-06 13:35:54 +0300

    testfiles

---
+++ testfiles/test_features/f-strings in Python.py
@@ -0,0 +1,29 @@
+v="ddff{test}dd"
+v=f"dd\"{test}dd{test}dd"
+v='ffdd{test}dd'
+v=f'dd{test}dd'
+v=f'dd\'{test}dddd{test more}dd'
+v=f'''ddddd{test}dd'''
+v=f"""ddddddddd{test}dd{t}dd{}dd{test}dd"""
+
+v = f"""{t}tt
+dddddddd{foo()}ddd
+      dddddd{foo()}ddd
+        dddd{foo()}ddd
+"""
+
+test
+
+f"""
+dddddddd{foo()}ddd
+      dddddd{foo()}ddd
+        dddd{foo()}ddd
+        dddd{foo()}ddd
+aa{a}        dddd{foo()}dd{ddd}ddd{d}ddddd
+        dddd{foo()}ddd
+        dddd{foo()}ddd
+        dddd{foo()}ddd
+        dddd{foo()}ddd
+"""
+
+test
