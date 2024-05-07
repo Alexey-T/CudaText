cfc88f0007cd3b5e3bb44af63cb2697e2078c5af
Alexey <support@uvviewsoft.com>
2024-05-07 11:43:01 +0300

    improve fuzzy search

--- app/proc_str.pas
+++ app/proc_str.pas
@@ -163,7 +163,7 @@ begin
 
   //calculate complex matches
   SetLength(Result, Length(SFindUpper));
-  for Deltas:= 0 to 63 do
+  for Deltas:= 0 to $FF do
     if TryMatch(Result, Deltas) then Exit;
   Result:= nil;
 end;
