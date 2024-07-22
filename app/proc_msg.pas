06227d003ae2b776e38365217287c2446861edff
Alexey T <support@uvviewsoft.com>
2024-07-22 09:14:31 +0300

    better btn title, fixing https://github.com/Alexey-T/CudaText/discussions/5627

--- app/proc_msg.pas
+++ app/proc_msg.pas
@@ -318,7 +318,7 @@ const
   msgConfirmFileDeletedOutside: string = 'File was deleted outside:';
   msgConfirmReloadIt: string = 'Reload it?';
   msgConfirmReloadYes: string = 'Reload';
-  msgConfirmReloadNoMore: string = 'No more notifications';
+  msgConfirmReloadNoMore: string = ' × '; //'No more notifications';
   msgConfirmReloadItHotkeysSess: string = '(Yes: reopen. No: open text from previous session.)';
   msgConfirmOpenCreatedDoc: string = 'Open created document?';
   msgConfirmSaveColorsToFile: string = 'Save theme to file?';
