4347cce6e93d770b3c75226cec60cd2014aef19f
Alexey T <support@uvviewsoft.com>
2024-09-07 07:16:29 +0300

    add qt6 to build script

--- setup/z_linux_intel.sh
+++ setup/z_linux_intel.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ./unix_common.sh linux gtk2 amd64 bin/linux-x64-gtk2 ~/cudatext_build
 ./unix_common.sh linux qt5 amd64 bin/linux-x64-qt5 ~/cudatext_build
+./unix_common.sh linux qt6 amd64 bin/linux-x64-qt6 ~/cudatext_build
 
 #./unix_common.sh linux gtk2 i386 bin/linux-x32-gtk2 ~/cudatext_build
 #./unix_common.sh linux qt5 i386 bin/linux-x32-qt5 ~/cudatext_build
