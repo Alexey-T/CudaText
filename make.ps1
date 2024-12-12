32cc1102629af78423806322ef76e9ffe7697446
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-12-12 15:50:03 +0200

    add github-actions

---
+++ make.ps1
@@ -0,0 +1,93 @@
+﻿#!/usr/bin/env pwsh
+##############################################################################################################
+
+Function Show-Usage {
+    Return "
+Usage: pwsh -File $($PSCommandPath) [OPTIONS]
+Options:
+    build   Build program
+"
+}
+
+Function Request-File {
+    ForEach ($REPLY in $args) {
+        $params = @{
+            Uri = $REPLY
+            OutFile = (Split-Path -Path $REPLY -Leaf).Split('?')[0]
+        }
+        Invoke-WebRequest @params | Out-Null
+        Return $params.OutFile
+    }
+}
+
+Function Install-Program {
+    While ($Input.MoveNext()) {
+        Switch ((Split-Path -Path $Input.Current -Leaf).Split('.')[-1]) {
+            'msi' {
+                & msiexec /passive /package $Input.Current | Out-Host
+            }
+            'exe' {
+                & ".\$($Input.Current)" /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART | Out-Host
+            }
+        }
+        Remove-Item $Input.Current
+    }
+}
+
+Function Build-Project {
+    $VAR = @{
+        Use = 'use'
+        Cmd = 'lazbuild'
+        Url = 'https://netix.dl.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%203.6/lazarus-3.6-fpc-3.2.2-win64.exe?viasf=1'
+        Path = "C:\Lazarus"
+    }
+    Try {
+        Get-Command $VAR.Cmd
+    } Catch {
+        Request-File $VAR.Url | Install-Program
+        $env:PATH+=";$($VAR.Path)"
+        Get-Command $VAR.Cmd
+    }
+    If (Test-Path -Path $($VAR.Use)) {
+        & git submodule update --init --recursive --force --remote | Out-Host
+        $COMPONENTS = "$($VAR.Use)\components.txt"
+        If (Test-Path -Path $COMPONENTS) {
+            Get-Content -Path $COMPONENTS | ForEach-Object {
+                If ((! (& $VAR.Cmd --verbose-pkgsearch $_ | Out-Null)) &&
+                    (! (& $VAR.Cmd --add-package $_ | Out-Null)) &&
+                    (! (Test-Path -Path "$($VAR.Use)\$($_)"))) {
+                        $OutFile = Request-File "https://packages.lazarus-ide.org/$($_).zip"
+                        Expand-Archive -Path $OutFile -DestinationPath "$($VAR.Use)\$($_)" -Force
+                        Remove-Item $OutFile
+                    }
+            }
+        }
+        Get-ChildItem -Filter '*.lpk' -Recurse -File –Path 'use' | ForEach-Object {
+            & $VAR.Cmd --add-package-link $_ | Out-Host
+        }
+    }
+    Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'app' | ForEach-Object {
+        & $VAR.Cmd --no-write-project --recursive --build-mode=release $_ | Out-Host
+    }
+}
+
+Function Switch-Action {
+    $ErrorActionPreference = 'stop'
+    Set-PSDebug -Strict -Trace 1
+    Invoke-ScriptAnalyzer -EnableExit -Path $PSCommandPath
+    If ($args.count -gt 0) {
+        Switch ($args[0]) {
+            'build' {
+                Build-Project
+            }
+            Default {
+                Show-Usage
+            }
+        }
+    } Else {
+        Show-Usage
+    }
+}
+
+##############################################################################################################
+Switch-Action @args | Out-Null
---
+++ make.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+function priv_clippit
+(
+    cat <<EOF
+Usage: bash ${0} [OPTIONS]
+Options:
+    build   Build program
+EOF
+)
+
+function priv_lazbuild
+(
+    if ! (which lazbuild); then
+        source '/etc/os-release'
+        case ${ID:?} in
+            debian | ubuntu)
+                sudo apt-get update
+                sudo apt-get install -y lazarus lcl-qt5
+                ;;
+        esac
+    fi
+    declare -r COMPONENTS='use/components.txt'
+    if [[ -d "${COMPONENTS%%/*}" ]]; then
+        git submodule update --init --recursive --force --remote
+        if [[ -f "${COMPONENTS}" ]]; then
+            while read -r; do
+                if [[ -n "${REPLY}" ]] &&
+                    ! (lazbuild --verbose-pkgsearch "${REPLY}") &&
+                    ! (lazbuild --add-package "${REPLY}") &&
+                    ! [[ -d "${COMPONENTS%%/*}/${REPLY}" ]]; then
+                        declare -A VAR=(
+                            [url]="https://packages.lazarus-ide.org/${REPLY}.zip"
+                            [out]=$(mktemp)
+                        )
+                        wget --output-document "${VAR[out]}" "${VAR[url]}" >/dev/null
+                        unzip -o "${VAR[out]}" -d "${COMPONENTS%%/*}/${REPLY}"
+                        rm --verbose "${VAR[out]}"
+                    fi
+            done < "${COMPONENTS}"
+        fi
+        find "${COMPONENTS%%/*}" -type 'f' -name '*.lpk' -exec \
+            lazbuild --add-package-link {} +
+    fi
+    find 'app' -type 'f' -name '*.lpi' -exec \
+        lazbuild --no-write-project --recursive --no-write-project --widgetset=qt {} + 1>&2
+)
+
+function priv_main
+(
+    set -euo pipefail
+    if ((${#})); then
+        case ${1} in
+            build) priv_lazbuild ;;
+            *) priv_clippit ;;
+        esac
+    else
+        priv_clippit
+    fi
+)
+
+priv_main "${@}" >/dev/null
