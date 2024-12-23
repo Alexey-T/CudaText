c4a191e4cd1ccbb82e9b379b7c763dcf3139585f
Alexey T <support@uvviewsoft.com>
2024-12-23 21:14:47 +0300

    add 2 shell scripts in 'doc'

---
+++ doc/git_clone_all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+git clone https://github.com/bgrabitmap/bgrabitmap
+git clone https://github.com/Alexey-T/python-for-lazarus
+git clone https://github.com/Alexey-T/atflatcontrols
+git clone https://github.com/Alexey-T/encconv
+git clone https://github.com/Alexey-T/atsynedit
+git clone https://github.com/Alexey-T/econtrol
+git clone https://github.com/Alexey-T/atsynedit_ex
+git clone https://github.com/Alexey-T/atsynedit_cmp
+git clone https://github.com/Alexey-T/atbinhex-lazarus
+git clone https://github.com/Alexey-T/emmet-pascal
+git clone https://github.com/Alexey-T/cudatext
