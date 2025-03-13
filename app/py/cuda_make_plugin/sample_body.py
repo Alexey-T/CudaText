ecbf92f9fb43eeb5b5302beae31034dc473c18cb
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2025-03-13 22:50:08 +0500

    [*] optimization of creation of new plugins

--- app/py/cuda_make_plugin/sample_body.py
+++ app/py/cuda_make_plugin/sample_body.py
@@ -1,11 +1,7 @@
         s = '''
         file lines count: {cnt}
-        option_int: {i}
-        option_bool: {b}
         '''.format(
-             cnt = ed.get_line_count(),
-             i = option_int,
-             b = option_bool,
-             )
+                cnt = ed.get_line_count()
+            )
         msg_box(s, MB_OK)
 
