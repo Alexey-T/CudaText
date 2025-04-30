e51aa6403f58086c8145bde186ef2648dd652551
Alexey T. <support@uvviewsoft.com>
2025-04-30 22:21:34 +0300

    fixing #5950

--- app/py/cuda_new_file/__init__.py
+++ app/py/cuda_new_file/__init__.py
@@ -13,7 +13,7 @@ class Command:
         if not files:
             msg_status(_('No files in data/newdoc'))
             return
-        
+
         infos = []
         for item in files:
             lex = lexer_proc(LEXER_DETECT, item)
@@ -22,29 +22,31 @@ class Command:
                     infos += [(item, l)]
             else:
                 infos += [(item, lex)]
-        
+
         lexers = sorted(list(set([item[1] for item in infos if item[1]])))
         if not lexers: return
-        
+
         res = dlg_menu(DMENU_LIST, lexers, caption=_('Templates'))
         if res is None: return
-        
+
         lexer = lexers[res]
         files = sorted([item[0] for item in infos if item[1]==lexer])
         if not files: return
-        
+
         if len(files)==1:
             fn = files[0]
         else:
             res = dlg_menu(DMENU_LIST, files, caption=_('Templates: {}').format(lexer))
             if res is None: return
             fn = files[res]
-            
+
         msg_status(_('New file from "{}", lexer "{}"').format(fn, lexer))
 
         fn = os.path.join(DIR_NEWDOC, fn)
+        s = open(fn, encoding='utf8').read()
+
         file_open('')
-        ed.set_text_all(open(fn).read())
-        
+        ed.set_text_all(s)
+
         if lexer:
             ed.set_prop(PROP_LEXER_FILE, lexer)
