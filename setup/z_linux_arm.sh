1409557cff63670d42da761f19e8edea207443c0
Alexey T <support@uvviewsoft.com>
2025-05-22 15:27:13 +0300

    fix dir for setup/aarch64-gtk2; add buildmodes for risc-v

--- setup/z_linux_arm.sh
+++ setup/z_linux_arm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./unix_common.sh linux gtk2 aarch64 bin/linux-aarch64 ~/cudatext_build
+./unix_common.sh linux gtk2 aarch64 bin/linux-aarch64-gtk2 ~/cudatext_build
 ./unix_common.sh linux qt5 aarch64 bin/linux-aarch64-qt5 ~/cudatext_build
 
 ./unix_common.sh linux gtk2 arm bin/linux-arm-gtk2 ~/cudatext_build
