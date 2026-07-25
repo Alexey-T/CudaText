a4b4cd4e5719c624d064c2fc63df65b880b90dc1
Alexey T <support@uvviewsoft.com>
2026-07-25 10:51:50 +0300

    fix url in prepare_build_dir.sh

--- setup/prepare_build_dir.sh
+++ setup/prepare_build_dir.sh
@@ -3,5 +3,5 @@ CUDDIR=~/cudatext_build
 mkdir $CUDDIR
 cd $CUDDIR
 
-curl -L https://sourceforge.net/projects/cudatext/files/release_libs/cudatext-windows-libs.zip/download >winlibs.zip
+curl -L https://sourceforge.net/projects/cudatext/files/libs_for_release_making/cudatext-windows-libs.zip/download >winlibs.zip
 unzip winlibs.zip -d .
