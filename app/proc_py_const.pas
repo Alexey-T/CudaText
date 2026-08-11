fa3a642b771dfbf0cf421fd74c5768b61c2673f1
Alexey T <support@uvviewsoft.com>
2026-08-12 00:49:22 +0300

    change int for prop_FN

--- app/proc_py_const.pas
+++ app/proc_py_const.pas
@@ -147,7 +147,6 @@ const
   EDSTATE_EXT_APPEARED = 21;
   EDSTATE_EXT_MODIFIED = 22;
 
-  PROP_FN             = -4;
   PROP_GUTTER_ALL     = -1;
   PROP_GUTTER_STATES  = 0;
   PROP_GUTTER_NUM     = 1;
@@ -201,6 +200,7 @@ const
   PROP_IN_SESSION = 49;
   PROP_COORDS = 50;
   PROP_ONE_LINE = 51;
+  PROP_FN       = 52;
   PROP_CODETREE = 54;
   PROP_EDITORS_LINKED = 55;
   PROP_KIND        = 59;
--- app/py/cudatext.py
+++ app/py/cudatext.py
@@ -154,7 +154,6 @@ LOG_CONSOLE_GET_MEMO_LINES  = 25
 LOG_PANEL_OUTPUT   = "0"
 LOG_PANEL_VALIDATE = "1"
 
-PROP_FN             = -4
 PROP_GUTTER_ALL     = -1
 PROP_GUTTER_STATES  = 0
 PROP_GUTTER_NUM     = 1
@@ -208,6 +207,7 @@ PROP_TAB_ID                = 48
 PROP_IN_SESSION            = 49
 PROP_COORDS                = 50
 PROP_ONE_LINE              = 51
+PROP_FN                    = 52
 PROP_CODETREE              = 54
 PROP_EDITORS_LINKED        = 55
 PROP_KIND                  = 59
