95d245f7c195abc57d32bb870964e6a53d3f9544
Alexey T <support@uvviewsoft.com>
2026-08-15 23:09:23 +0300

    api Canvas_image

--- app/proc_py_const.pas
+++ app/proc_py_const.pas
@@ -594,6 +594,7 @@ const
   CANVAS_TEXT          = 20;
   CANVAS_LINE          = 21;
   CANVAS_BITMAP        = 22;
+  CANVAS_IMAGE         = 23;
   CANVAS_PIXEL         = 24;
   CANVAS_RECT          = 30;
   CANVAS_RECT_FRAME    = 31;
