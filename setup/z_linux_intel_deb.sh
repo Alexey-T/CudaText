c8f6094dfd98cccc41c9768b091f8459ef878efd
Alexey T <support@uvviewsoft.com>
2026-08-21 17:46:41 +0300

    fix deb-script

--- setup/z_linux_intel_deb.sh
+++ setup/z_linux_intel_deb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./unix_common_deb.sh gtk2_amd64 x64-gtk2
-./unix_common_deb.sh gtk3_amd64 x64-gtk3
-./unix_common_deb.sh qt5_amd64 x64-qt5
-./unix_common_deb.sh qt6_amd64 x64-qt6
+./unix_common_deb.sh gtk2_amd64 amd64-gtk2
+./unix_common_deb.sh gtk3_amd64 amd64-gtk3
+./unix_common_deb.sh qt5_amd64 amd64-qt5
+./unix_common_deb.sh qt6_amd64 amd64-qt6
