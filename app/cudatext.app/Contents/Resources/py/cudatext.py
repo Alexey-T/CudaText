91d98647226d379b33ca4ff32cfc0fe5cb507ec6
Alexey T <support@uvviewsoft.com>
2026-08-25 15:27:53 +0300

    api get_text_all has new param 'no_ends', #6430

--- app/cudatext.app/Contents/Resources/py/cudatext.py
+++ app/cudatext.app/Contents/Resources/py/cudatext.py
@@ -1411,8 +1411,8 @@ class Editor:
     def get_char_count(self, max_chars, max_time):
         return ct.ed_get_char_count(self.h, max_chars, max_time)
 
-    def get_text_all(self):
-        return ct.ed_get_text_all(self.h)
+    def get_text_all(self, no_ends=True):
+        return ct.ed_get_text_all(self.h, no_ends)
 
     def set_text_all(self, text):
         return ct.ed_set_text_all(self.h, esc_z(text))
